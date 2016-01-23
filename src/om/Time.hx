7c08b927cec26d4c5e2c9962f4d582e2ac68492f
Tong <tong@disktree.net>
2016-01-23 22:09:26 +0100

    Fix

--- src/om/Time.hx
+++ src/om/Time.hx
@@ -4,15 +4,15 @@ class Time {
 
 	public static inline function now() : Float {
 
-		#if nodejs
+		#if sys
+		return Sys.time();
+
+		#elseif nodejs
 		var hr = js.Node.process.hrtime();
 		return ( hr[0] * 1e9 + hr[1] ) / 1e6;
 
 		#elseif js
 		return untyped window.performance.now();
-
-		#elseif sys
-		return Sys.time();
 
 		#end
 	}
