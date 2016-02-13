bebc8da37c29e13316d22c8653988784463b7dc5
Tong <tong@disktree.net>
2016-02-13 17:58:45 +0100

    More system features detection methods

--- src/om/System.hx
+++ src/om/System.hx
@@ -4,9 +4,7 @@ import js.Browser.navigator;
 
 class System {
 
-    public static var language(get,never) : String;
-
-    static inline function get_language() : String {
+    public static inline function getLanguage() : String {
 		return untyped window.navigator.language;
 	}
 
@@ -18,4 +16,67 @@ class System {
     public static function isMobile() : Bool {
         return ~/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.match( navigator.userAgent );
     }
+
+    public static function detectWebAudio() : Bool {
+		try {
+			if( untyped window.AudioContext == null ) //|| window.webkitAudioContext || window.mozAudioContext )
+				return false;
+		} catch(e:Dynamic) {
+			return false;
+		}
+		return true;
+	}
+
+    public static function detectWebGL() : Bool {
+		try {
+			if( untyped window.WebGLRenderingContext == null )
+                return false;
+			if( document.createCanvasElement().getContextWebGL() == null )
+                return false;
+		} catch(e:Dynamic) {
+			return false;
+		}
+		return true;
+	}
+
+	public static function detectPointerlock() : Bool {
+		try {
+			return untyped __js__('"pointerLockElement" in document||"mozPointerLockElement" in document||"webkitPointerLockElement" in document');
+		} catch(e:Dynamic) {
+			return false;
+		}
+		return true;
+	}
+
+    public static inline function detectSessionStorage() : Bool {
+		try {
+			return window.sessionStorage.getItem != null;
+		} catch(e:Dynamic) {
+			return false;
+		}
+	}
+
+    public static inline function detectGamepad() : Bool {
+		return untyped !!navigator.webkitGetGamepads || !!navigator.webkitGamepads || !!navigator.getGamepads;
+	}
+
+    public static inline function detectFile() : Bool {
+		return untyped __js__('!! window.File && !! window.FileReader && !! window.FileList && !! window.Blob');
+	}
+
+	public static inline function detectFileSystem() : Bool {
+		return untyped __js__('!! window.File && !! window.FileReader && !! window.FileList && !! window.Blob');
+	}
+
+	public static inline function detectUserMedia() : Bool {
+		return untyped __js__('!! window.navigator.getUserMedia || !! window.navigator.webkitGetUserMedia || !! window.navigator.mozGetUserMedia || !! window.navigator.msGetUserMedia');
+	}
+
+	public static inline function detectRequestAnimationFrame() : Bool {
+		return untyped __js__('!! window.mozRequestAnimationFrame || !! window.webkitRequestAnimationFrame || !! window.oRequestAnimationFrame || !! window.msRequestAnimationFrame');
+	}
+
+	public static inline function detectWorker() : Bool {
+		return untyped __js__( '!! window.Worker' );
+	}
 }
