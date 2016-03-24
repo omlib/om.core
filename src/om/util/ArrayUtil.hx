1b873b0909a5fcd090419001c9e0d6328ddb0ff9
Tong <tong@disktree.net>
2016-03-24 17:20:08 +0100

    ArrayUtil.shuffle

--- src/om/util/ArrayUtil.hx
+++ src/om/util/ArrayUtil.hx
@@ -6,16 +6,30 @@ import haxe.macro.Expr;
 
 class ArrayUtil {
 
+	/**
+		Pushes value to array and returns the array.
+	*/
+	public static inline function add<T>( arr : Array<T>, v : T ) : Array<T> {
+		arr.push( v );
+		return arr;
+	}
+
+	/**
+	*/
 	public static function any<T>( arr : Array<T>, predicate : T->Bool ) {
 		for( e in arr ) if( predicate(e) ) return true;
 		return false;
 	}
 
+	/**
+	*/
 	public static inline function append<T>( a : Array<T>, b : Array<T> ) : Array<T> {
-		a = a.concat(b);
+		a = a.concat( b );
 		return a;
 	}
 
+	/**
+	*/
 	public static function contains<T>( arr : Array<T>, element : T, ?eq : T->T->Bool ) : Bool {
 		if( eq == null )
 			return arr.indexOf( element ) >= 0;
@@ -25,11 +39,35 @@ class ArrayUtil {
 		return false;
 	}
 
+	/**
+		Returns the last value of given array.
+	*/
 	public static inline function last<T>( a : Array<T> ) : T
 		return a[a.length-1];
 
-	public static function sorted<T>( array : Array<T>, f : T->T->Int ) : Array<T> {
-		var n = array.copy();
+	/**
+		Shuffles given array in place.
+	*/
+	public static function shuffle<T>( arr : Array<T> ) : Array<T> {
+		var x : T;
+		var x : T, j : Int, i = arr.length;
+		while( i > 0 ) {
+			j = Math.floor( Math.random() * i );
+			x = arr[i-1];
+			arr[i-1] = arr[j];
+			arr[j] = x;
+			i--;
+		}
+		return arr;
+	}
+
+	// https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
+	//public static function shuffleFisherYates<T>( arr : Array<T> ) : Array<T> {
+
+	/**
+	*/
+	public static function sorted<T>( arr : Array<T>, f : T->T->Int ) : Array<T> {
+		var n = arr.copy();
     	n.sort( f );
     	return n;
 	}
@@ -40,6 +78,8 @@ class ArrayUtil {
 	public inline static function with<T>( arr : Array<T>, e : T ) : Array<T>
 		return arr.concat( [e] );
 
+	/**
+	*/
 	macro public static function pluck<T,TOut>( arr : ExprOf<Array<T>>, expr : ExprOf<TOut> ) : ExprOf<Array<TOut>> {
 		return macro $e{arr}.map( function(_) return ${expr} );
 	}
