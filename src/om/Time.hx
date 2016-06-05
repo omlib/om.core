a8b688995988fba14229cec1341373401830b83f
tong <tong@disktree.net>
2016-06-05 05:49:20 +0200

    fix

--- src/om/Time.hx
+++ src/om/Time.hx
@@ -2,38 +2,9 @@ package om;
 
 class Time {
 
-	#if (sys||nodejs)
-
-	static inline function __init__() {
-		#if sys
-		startTime = Sys.time();
-		#elseif nodejs
-		startTime = getNanoSeconds();
-		#end
-	}
-
-	#end
-
-	public static var startTime(default,null) : Float;
-
-	/*
-	#if sys
-	public static var startTime(default,null) : Float; //= Sys.time();
-
-	#elseif nodejs
-	public static var startTime(default,null) = getNanoSeconds();
-	#end
-	*/
-
-	//public static var start(default,null) = now();
-
 	public static inline function now() : Float {
 
 		#if sys
-		//return Sys.cpuTime();
-		//trace(Sys.time()-start);
-		//return (Sys.time() - start) * 1000;
-		//trace(Sys.time()-startTime);
 		return Sys.time() * 1000;
 
 		#elseif nodejs
@@ -42,14 +13,23 @@ class Time {
 		#elseif js
 		return untyped window.performance.now();
 
+		#else
+		return throw new om.error.NotImplemented();
+
 		#end
 	}
 
 	#if nodejs
 
+	public static var startTime(default,null) : Float;
+
 	public static inline function getNanoSeconds() : Float {
 		var hr = js.Node.process.hrtime();
 		return hr[0] * 1e9 + hr[1];
+	}
+
+	static inline function __init__() {
+		startTime = getNanoSeconds();
 	}
 
 	#end
