b432790a6fc44ea5409c8ac28ef79a52df5a93b7
tong <tong@disktree.net>
2016-09-16 19:55:55 +0200

    Fix

--- src/om/Time.hx
+++ src/om/Time.hx
@@ -1,11 +1,13 @@
 package om;
 
-class Time {
+abstract Time(Float) {
+
+	public inline function new( v : Float ) this = v;
 
 	public static inline function now() : Float {
 
 		#if sys
-		return Sys.time() * 1000;
+		return (Sys.time() - startTime) * 1000;
 
 		#elseif nodejs
 		return getNanoSeconds() / 1e6;
@@ -19,19 +21,18 @@ class Time {
 		#end
 	}
 
-	#if nodejs
+	#if sys
 
-	public static var startTime(default,null) : Float;
+	public static var startTime(default,null) : Float = Sys.time();
+
+	#elseif nodejs
+
+	public static var startTime(default,null) : Float = getNanoSeconds();
 
 	public static inline function getNanoSeconds() : Float {
-		var hr = js.Node.process.hrtime();
-		return hr[0] * 1e9 + hr[1];
-	}
-
-	static inline function __init__() {
-		startTime = getNanoSeconds();
+		var t = js.Node.process.hrtime();
+		return t[0] * 1e9 + t[1];
 	}
 
 	#end
-
 }
