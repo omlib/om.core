197cd595f71c91fb17ff8ed428f27beb6ce1f031
tong <tong@disktree.net>
2016-09-16 19:46:19 +0200

    Cleanup

--- src/om/Console.hx
+++ src/om/Console.hx
@@ -1,19 +1,6 @@
 package om;
 
-typedef Console =
-	#if sys
-	om._sys.Console;
-	#elseif js
-		#if nodejs
-		om._node.Console;
-		#else
-		om._web.Console;
-		#end
-	#end
-
-/*
 #if macro
-//#
 #elseif js
 	#if nodejs
 	import js.Node.process;
@@ -33,9 +20,9 @@ import om.ANSI;
     haxe/node   => stdout
     haxe/sys    => stdout
 
-    Define -D no_console to remove all calls to this class.
+    Set -D no_console to remove all calls to this class.
 
-* /
+*/
 @defines('no_console','Remove calls to om.Console')
 class Console {
 
@@ -67,65 +54,68 @@ class Console {
     public static inline function warn( o : Dynamic ) println( Std.string(o), color_warn );
     public static inline function error( o : Dynamic ) println( Std.string(o), color_error );
 
-    #elseif nodejs
+	#elseif js
 
-    static function __print( str : String, ?color : Int ) {
-        #if (!no_console&&!doc_gen)
-        if( !noColors && color != null ) str = ANSI.colorize( str, color );
-        process.stdout.write( str );
-        #end
-    }
+		#if nodejs
 
-    public static inline function print( obj : Dynamic, ?color : Int ) __print( Std.string(obj), color );
-    public static inline function println( obj : Dynamic, ?color : Int ) __print( Std.string(obj)+'\n', color );
-    public static inline function clear() process.stdout.write( '\033c' );
-    public static inline function ln() process.stdout.write( '\n' );
+		static function __print( str : String, ?color : Int ) {
+	        #if (!no_console&&!doc_gen)
+	        if( !noColors && color != null ) str = ANSI.colorize( str, color );
+	        process.stdout.write( str );
+	        #end
+	    }
 
-    public static inline function log( obj : Dynamic ) println( obj );
-    public static inline function info( obj : Dynamic ) println( obj, color_info );
-    public static inline function debug( obj : Dynamic ) println( obj, color_debug );
-    public static inline function warn( obj : Dynamic ) println( obj, color_warn );
-    public static inline function error( obj : Dynamic ) println( obj, color_error );
+		public static inline function print( obj : Dynamic, ?color : Int ) __print( Std.string(obj), color );
+	    public static inline function println( obj : Dynamic, ?color : Int ) __print( Std.string(obj)+'\n', color );
+	    public static inline function clear() process.stdout.write( '\033c' );
+	    public static inline function ln() process.stdout.write( '\n' );
 
-    #elseif js
+	    public static inline function log( obj : Dynamic ) println( obj );
+	    public static inline function info( obj : Dynamic ) println( obj, color_info );
+	    public static inline function debug( obj : Dynamic ) println( obj, color_debug );
+	    public static inline function warn( obj : Dynamic ) println( obj, color_warn );
+	    public static inline function error( obj : Dynamic ) println( obj, color_error );
 
-    public static inline function print( obj : Dynamic ) { #if (!no_console&&!doc_gen) console.log( obj ); #end }
-    public static inline function println( obj : Dynamic ) { #if (!no_console&&!doc_gen) console.log( obj ); #end }
-	public static inline function clear() { #if (!no_console&&!doc_gen) console.clear(); #end }
-    public static inline function ln() log( '' );
+		#else
 
-    public static inline function log( obj : Dynamic ) { #if (!no_console&&!doc_gen) console.log( obj ); #end }
-	public static inline function info( obj : Dynamic ) { #if (!no_console&&!doc_gen) console.info( obj ); #end }
-	public static inline function debug( obj : Dynamic ) { #if (!no_console&&!doc_gen) console.debug( obj ); #end }
-	public static inline function warn( obj : Dynamic ) { #if (!no_console&&!doc_gen) console.warn( obj ); #end }
-	public static inline function error( obj : Dynamic ) { #if (!no_console&&!doc_gen) console.error( obj ); #end }
+		public static inline function print( obj : Dynamic ) { #if (!no_console&&!doc_gen) console.log( obj ); #end }
+	    public static inline function println( obj : Dynamic ) { #if (!no_console&&!doc_gen) console.log( obj ); #end }
+		public static inline function clear() { #if (!no_console&&!doc_gen) console.clear(); #end }
+	    public static inline function ln() log( '' );
 
-	public static inline function table( obj : Dynamic ) { #if (!no_console&&!doc_gen) console.table( obj ); #end }
+	    public static inline function log( obj : Dynamic ) { #if (!no_console&&!doc_gen) console.log( obj ); #end }
+		public static inline function info( obj : Dynamic ) { #if (!no_console&&!doc_gen) console.info( obj ); #end }
+		public static inline function debug( obj : Dynamic ) { #if (!no_console&&!doc_gen) console.debug( obj ); #end }
+		public static inline function warn( obj : Dynamic ) { #if (!no_console&&!doc_gen) console.warn( obj ); #end }
+		public static inline function error( obj : Dynamic ) { #if (!no_console&&!doc_gen) console.error( obj ); #end }
 
-    //public static inline function trace( obj : Dynamic ) { #if (!no_console&&!doc_gen) untyped console.trace( obj ); #end }
+		public static inline function table( obj : Dynamic ) { #if (!no_console&&!doc_gen) console.table( obj ); #end }
 
-    public static inline function assert( expression : Dynamic, obj : Dynamic ) { #if (!no_console&&!doc_gen) console.assert( expression, obj ); #end }
-    public static inline function exception( obj : Dynamic ) { #if (!no_console&&!doc_gen) console.exception( obj ); #end }
+	    //public static inline function trace( obj : Dynamic ) { #if (!no_console&&!doc_gen) untyped console.trace( obj ); #end }
 
-	public static inline function count( label : String ) { #if (!no_console&&!doc_gen) console.count( label ); #end }
+	    public static inline function assert( expression : Dynamic, obj : Dynamic ) { #if (!no_console&&!doc_gen) console.assert( expression, obj ); #end }
+	    public static inline function exception( obj : Dynamic ) { #if (!no_console&&!doc_gen) console.exception( obj ); #end }
 
-	public static inline function dir( obj : Dynamic ) { #if (!no_console&&!doc_gen) console.dir( obj ); #end }
-	public static inline function dirxml( obj : Dynamic ) { #if (!no_console&&!doc_gen)  untyped console.dirxml( obj ); #end }
+		public static inline function count( label : String ) { #if (!no_console&&!doc_gen) console.count( label ); #end }
 
-	public static inline function group( obj : Dynamic ) { #if (!no_console&&!doc_gen) console.group( obj ); #end }
-	public static inline function groupCollapsed( obj : Dynamic ) { #if (!no_console&&!doc_gen) console.groupCollapsed( obj ); #end }
-	public static inline function groupEnd() { #if (!no_console&&!doc_gen) console.groupEnd(); #end }
+		public static inline function dir( obj : Dynamic ) { #if (!no_console&&!doc_gen) console.dir( obj ); #end }
+		public static inline function dirxml( obj : Dynamic ) { #if (!no_console&&!doc_gen)  untyped console.dirxml( obj ); #end }
 
-	public static inline function profile( label : String ) { #if (!no_console&&!doc_gen) console.profile( label ); #end }
-	public static inline function profileEnd() { #if (!no_console&&!doc_gen) console.profileEnd(); #end }
+		public static inline function group( obj : Dynamic ) { #if (!no_console&&!doc_gen) console.group( obj ); #end }
+		public static inline function groupCollapsed( obj : Dynamic ) { #if (!no_console&&!doc_gen) console.groupCollapsed( obj ); #end }
+		public static inline function groupEnd() { #if (!no_console&&!doc_gen) console.groupEnd(); #end }
 
-	public static inline function time( label : String ) { #if (!no_console&&!doc_gen) console.time( label ); #end }
-	public static inline function timeEnd( label : String ) { #if (!no_console&&!doc_gen) console.timeEnd( label ); #end }
+		public static inline function profile( label : String ) { #if (!no_console&&!doc_gen) console.profile( label ); #end }
+		public static inline function profileEnd() { #if (!no_console&&!doc_gen) console.profileEnd(); #end }
 
-	public static inline function timeline( label : String ) { #if (!no_console&&!doc_gen) untyped console.timeline( label ); #end }
-	public static inline function timelineEnd( label : String ) { #if (!no_console&&!doc_gen)  untyped console.timelineEnd( label ); #end }
-	public static inline function timeStamp( label : String ) { #if (!no_console&&!doc_gen)  untyped console.timeStamp( label ); #end }
+		public static inline function time( label : String ) { #if (!no_console&&!doc_gen) console.time( label ); #end }
+		public static inline function timeEnd( label : String ) { #if (!no_console&&!doc_gen) console.timeEnd( label ); #end }
+
+		//public static inline function timeline( label : String ) { #if (!no_console&&!doc_gen) untyped console.timeline( label ); #end }
+		//public static inline function timelineEnd( label : String ) { #if (!no_console&&!doc_gen)  untyped console.timelineEnd( label ); #end }
+		//public static inline function timeStamp( label : String ) { #if (!no_console&&!doc_gen)  untyped console.timeStamp( label ); #end }
+
+		#end
 
     #end
 }
-*/
