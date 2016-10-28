5a85780d885c462c449bfbd710b276bf39d6ed57
tong <tong@disktree.net>
2016-10-28 15:06:48 +0200

    Add Any type (from tink_core)

---
+++ src/om/Any.hx
@@ -0,0 +1,12 @@
+package om;
+
+/**
+	Safer version of Haxe Dynamic that prevents common issues with Dynamics by forcing
+	you to explicitly convert it to the desired type before doing any operations on it.
+
+	- https://github.com/haxetink/tink_core#any
+	- https://github.com/haxetink/tink_core/blob/master/src/tink/core/Any.hx
+**/
+abstract Any(Dynamic) from Dynamic {
+	@:noCompletion @:to inline function __promote<A>() : A return this;
+}
