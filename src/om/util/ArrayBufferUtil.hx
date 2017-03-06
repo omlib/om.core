c1cdee0eaffbcad28f6a5ab3594850eb30d17a11
tong <tong@disktree.net>
2017-03-07 00:10:40 +0100

    Cleanup

--- src/om/util/ArrayBufferUtil.hx
+++ src/om/util/ArrayBufferUtil.hx
@@ -1,53 +1,28 @@
 package om.util;
 
-#if js
-
 import js.html.ArrayBuffer;
+import js.html.Uint8Array;
 
 class ArrayBufferUtil {
 
 	/**
-		str2ab
+		String to ArrayBuffer.
 	*/
 	public static function str2ab( str : String ) : ArrayBuffer {
 		var b = new ArrayBuffer( str.length );
-		var v = new js.html.Uint8Array( b );
+		var v = new Uint8Array( b );
 		for( i in 0...str.length ) v[i] = str.charCodeAt( i );
 		return b;
 	}
 
 	/**
-		ab2str
+		ArrayBuffer to String.
 	*/
 	public static function ab2str( buf : ArrayBuffer ) : String {
-		var v = new js.html.Uint8Array( buf );
+		var v = new Uint8Array( buf );
 		var s = "";
 		for( i in 0...v.length ) s += String.fromCharCode( v[i] );
 		return s;
 	}
 
-	/*
-	public static function toArrayBuffer( t : String, cb : ArrayBuffer->Void ) {
-		var bb = new BlobBuilder();
-		bb.append(t);
-		var f = new FileReader();
-		f.onload = function(e) {
-			cb(e.target.result);
-		}
-		f.readAsArrayBuffer(bb.getBlob());
-	}
-
-	public static function toString( b : ArrayBuffer, cb : String->Void ) {
-		var bb = new BlobBuilder();
-		bb.append(b);
-		var f = new FileReader();
-		f.onload = function(e) {
-			cb(e.target.result);
-		}
-		f.readAsText(bb.getBlob());
-	}
-	*/
-
 }
-
-#end
