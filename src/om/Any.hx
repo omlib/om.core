2eaf57e4cf7b37c424257b32986bfa756e0bab51
tong <tong@disktree.net>
2017-08-17 11:41:56 +0200

    Mark om.Any as depricated if haxe >= 3.4

--- src/om/Any.hx
+++ src/om/Any.hx
@@ -7,6 +7,9 @@ package om;
 	- https://github.com/haxetink/tink_core#any
 	- https://github.com/haxetink/tink_core/blob/master/src/tink/core/Any.hx
 **/
+#if (haxe_ver >= 3.4)
+@:deprecated
+#end
 abstract Any(Dynamic) from Dynamic {
 	@:noCompletion @:to inline function __promote<A>() : A return this;
 }
