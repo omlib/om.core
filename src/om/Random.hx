c9547d64444a06ddc35edb9e449bc1d4d9898040
tong <tong@disktree.net>
2017-08-28 14:19:00 +0200

    Cleanup

--- src/om/Random.hx
+++ src/om/Random.hx
@@ -2,14 +2,11 @@ package om;
 
 abstract Random(Float) from Float to Float {
 
-	public inline function new( f : Float )
-		this = Math.random() * f;
+	public inline function new( seed : Float )
+		this = Math.random() * seed;
 
 	@:to public inline function toInt() : Int
 		return Std.int( this );
-
-	public static inline function int( factor : Float ) : Int
-		return Std.int( new Random( factor ) );
 
 	/**
 		Returns random Bool.
@@ -17,6 +14,30 @@ abstract Random(Float) from Float to Float {
 	public static inline function bool( factor = 0.5 ) : Bool
 		return factor < (Math.random() * 1.0);
 
+	/**
+		Returns random Int.
+	*/
+	public static inline function int( seed : Float ) : Int
+		return Std.int( new Random( seed ) );
+
+
+	/*
 	public static inline function getRandomValues( factor = 0.5 ) : Bool
 		return factor < (Math.random() * 1.0);
+	*/
+
+	/*
+	public static function getRandomString( length : Int ) : String {
+
+		#if nodejs
+        return js.node.Crypto.randomBytes( length ).toString( 'hex' ).substr( 0, length );
+
+		var charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        var values = new js.html.Uint32Array( length );
+        window.crypto.getRandomValues( values );
+        var str = "";
+        for( i in 0...length ) result += charset.charAt( values[i] % charset.length );
+        return str;
+	}
+	*/
 }
