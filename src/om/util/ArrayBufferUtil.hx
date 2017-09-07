8f6e14b265954e7bd10d9e22eeef31d3a4d19d3c
tong <tong@disktree.net>
2017-09-07 04:09:40 +0200

    Cleanup

--- src/om/util/ArrayBufferUtil.hx
+++ src/om/util/ArrayBufferUtil.hx
@@ -1,26 +1,14 @@
 package om.util;
 
 #if js
+
 import js.html.ArrayBuffer;
 import js.html.Uint8Array;
-#end
 
 class ArrayBufferUtil {
 
-	#if js
-
 	/**
-		String to ArrayBuffer.
-	*/
-	public static function str2ab( str : String ) : ArrayBuffer {
-		var b = new ArrayBuffer( str.length );
-		var v = new Uint8Array( b );
-		for( i in 0...str.length ) v[i] = str.charCodeAt( i );
-		return b;
-	}
-
-	/**
-		ArrayBuffer to String.
+		js.html.ArrayBuffer to String.
 	*/
 	public static function ab2str( buf : ArrayBuffer ) : String {
 		var v = new Uint8Array( buf );
@@ -35,13 +23,23 @@ class ArrayBufferUtil {
 		js.node.Buffer to js.html.ArrayBuffer.
 	*/
 	public static function buf2ab( buf : js.node.Buffer ) : ArrayBuffer {
-	    var ab = new ArrayBuffer( buf.length );
-	    var view = new Uint8Array(ab);
+		var ab = new ArrayBuffer( buf.length );
+		var view = new Uint8Array(ab);
 		for( i in 0...buf.length ) view[i] = buf[i];
-	    return ab;
+		return ab;
 	}
 
-	#end // nodejs
-	#end // js
+	#end
+
+	/**
+		String to js.html.ArrayBuffer.
+	*/
+	public static function str2ab( str : String ) : ArrayBuffer {
+		var b = new ArrayBuffer( str.length );
+		var v = new Uint8Array( b );
+		for( i in 0...str.length ) v[i] = str.charCodeAt( i );
+		return b;
+	}
 
 }
+#end
