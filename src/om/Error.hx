c73b1da8fda02ad9d86169626ca29f8af53495ea
tong <tong@disktree.net>
2017-09-07 03:51:09 +0200

    Cleanup

--- src/om/Error.hx
+++ src/om/Error.hx
@@ -12,27 +12,36 @@ class Error #if js extends js.Error #end {
     public var name(default,null) : String;
     #end
 
-	public var fileName(get,null) : String;
+	public var fileName(get,never) : String;
 	inline function get_fileName() return pos.fileName;
 
-	public var lineNumber(get,null) : Int;
+	public var lineNumber(get,never) : Int;
 	inline function get_lineNumber() return pos.lineNumber;
 
-	public var methodName(get,null) : String;
+	public var methodName(get,never) : String;
 	inline function get_methodName() return pos.methodName;
 
-	public function new( ?message : String, ?pos : PosInfos ) {
+	public var stackItems(default,null) : Array<StackItem>;
+
+	//public function new( ?message : String, ?pos : PosInfos ) {
+	public function new( ?message : String, ?stack : Array<StackItem>, ?pos : PosInfos ) {
 
 		#if js
 		super( message );
 		#end
-
 		this.message = message;
 		this.pos = pos;
 
 		var clName = Type.getClassName( Type.getClass(this) );
 		if( clName.indexOf( '.' ) != -1 ) clName = clName.split('.').pop();
 		this.name = clName;
+
+		if( stack == null ) {
+			stack = try CallStack.exceptionStack() catch(e:Dynamic) [];
+			if( stack.length == 0 )
+				stack = try CallStack.callStack() catch(e:Dynamic) [];
+		}
+		this.stackItems = stack;
 	}
 
 	public function toString() : String {
@@ -41,32 +50,31 @@ class Error #if js extends js.Error #end {
 		return getSourcePosition() + ' : ' + message + CallStack.toString( items );
 	}
 
-	public inline function getSourcePosition() : String
-	    return pos.fileName + ':' + pos.lineNumber;
-
 	public inline function getCallStack() : Array<StackItem>
 		return CallStack.callStack();
-
-	public inline function callStack() : String
-		return CallStack.toString( CallStack.callStack() );
 
 	public inline function getExceptionStack() : Array<StackItem>
 		return CallStack.exceptionStack();
 
+	public inline function getSourcePosition() : String
+		return pos.fileName + ':' + pos.lineNumber;
+
+	/*
+	public inline function callStack() : String
+		return CallStack.toString( CallStack.callStack() );
+
 	public inline function exceptionStack() : String
 		return CallStack.toString( CallStack.exceptionStack() );
 
-	//public function getStackString() : String
-	//    return CallStack.toString( items );
-}
 
-/*
-abstract Error(TError) to TError {
+	public inline function getSourcePosition() : String
+		return pos.fileName + ':' + pos.lineNumber;
+		*/
 
-	public inline function new( e : TError ) this = e;
-
-	@:From public static inline function fromString( str : String ) : Error {
-		return new Error( new TError( str ) );
+		/*
+	public static inline function ofDynamic( err : Dynamic, ?pos : PosInfos ) : TError {
+		return new TError( err+'', null, pos );
 	}
+	*/
+
 }
-*/
