6903511d848042de5b1c73334bad53baef640df6
tong <tong@disktree.net>
2017-09-28 00:30:57 +0200

    Cleanup

--- src/om/util/ArrayUtil.hx
+++ src/om/util/ArrayUtil.hx
@@ -6,33 +6,54 @@ import haxe.macro.Expr;
 
 class ArrayUtil {
 
-	/**
+	//See: https://github.com/polygonal/ds/blob/master/src/de/polygonal/ds/tools/NativeArrayTools.hx
+
+	/*
+	public static inline function alloc<T>( len : Int ) : Array<T> {
+		#if js
+		return untyped __js__("new Array({0})", len );
+		#elseif neko
+		return untyped __dollar__amake( len );
+		#elseif cs
+		return cs.Lib.arrayAlloc( len );
+		#elseif java
+		return untyped Array.alloc( len );
+		#elseif cpp
+		var a = new Array<T>();
+		cpp.NativeArray.setSize( a, len );
+		return a;
+		#elseif python
+		return python.Syntax.pythonCode( "[{0}]*{1}", null, len );
+		#else
+		var a = [];
+		untyped a.length = len;
+		return a;
+		#end
+	}
 	*/
-	public static inline function before<T>( arr : Array<T>, e : T ) : Array<T> {
-		return arr.slice( 0, arr.indexOf( e ) );
+
+	/**
+		Pushes value to array and returns the array.
+	*/
+	public static inline function add<T>( arr : Array<T>, v : T ) : Array<T> {
+		arr.push( v );
+		return arr;
 	}
 
 	/**
-		Returns all the elements after the first occurrance of `e`.
+		Returns all the items after the first occurrance of `e`.
 	*/
 	public static inline function after<T>( arr : Array<T>, e : T ) : Array<T> {
-    	return arr.slice( arr.indexOf( e ) + 1 );
+		return arr.slice( arr.indexOf( e ) + 1 );
 	}
 
 	/**
-		Returns a copy of the array with the new element added to the end.
-	*/
-	public inline static function with<T>( a : Array<T>, e : T ) : Array<T> {
-		return a.concat( [e] );
-	}
-
-	/**
-		Returns true if predicate return true for all elements in the array.
+		Returns true if predicate return true for all items in the array.
 	*/
 	public static function all<T>( arr : Array<T>, predicate : T->Bool ) : Bool {
 		for( e in arr )
-		if( !predicate( e ) )
-		return false;
+			if( !predicate( e ) )
+				return false;
 		return true;
 	}
 
@@ -41,23 +62,39 @@ class ArrayUtil {
 	*/
 	public static function any<T>( arr : Array<T>, predicate : T->Bool ) : Bool {
 		for( e in arr )
-		if( predicate( e ) )
-		return true;
-		return false;
-	}
-
-	/**
-	*/
-	public static function contains<T>( arr : Array<T>, element : T, ?eq : T->T->Bool ) : Bool {
-		if( eq == null )
-			return arr.indexOf( element ) >= 0;
-		for( i in 0...arr.length )
-			if( eq( arr[i], element ) )
+			if( predicate( e ) )
 				return true;
 		return false;
 	}
 
 	/**
+	*/
+	public static inline function append<T>( a : Array<T>, b : Array<T> ) : Array<T> {
+		a = a.concat( b );
+		return a;
+	}
+
+	/**
+		Returns all items before `e`.
+	*/
+	public static inline function before<T>( arr : Array<T>, e : T ) : Array<T> {
+		return arr.slice( 0, arr.indexOf( e ) );
+	}
+
+	/**
+		Returns `true` if `e` is found in the array.
+	*/
+	public static function contains<T>( arr : Array<T>, e : T, ?eq : T->T->Bool ) : Bool {
+		if( eq == null )
+			return arr.indexOf( e ) >= 0;
+		for( i in 0...arr.length )
+			if( eq( arr[i], e ) )
+				return true;
+		return false;
+	}
+
+	/**
+		Returns `true` if all items match.
 	*/
 	public static function equals<T>( a : Array<T>, b : Array<T>, f : T->T->Bool ) : Bool {
 		if( a == null || b == null || a.length != b.length )
@@ -69,18 +106,15 @@ class ArrayUtil {
 	}
 
 	/**
-		Pushes value to array and returns the array.
 	*/
-	public static inline function add<T>( arr : Array<T>, v : T ) : Array<T> {
-		arr.push( v );
-		return arr;
+	public static inline function dropLeft<T>( a: Array<T>, n : Int ) : Array<T> {
+		return (n >= a.length) ? [] : a.slice( n );
 	}
 
 	/**
 	*/
-	public static inline function append<T>( a : Array<T>, b : Array<T> ) : Array<T> {
-		a = a.concat( b );
-		return a;
+	public static inline function dropRight<T>( a: Array<T>, n : Int ) : Array<T> {
+		return (n >= a.length) ? [] : a.slice( 0, a.length - n );
 	}
 
 	/**
@@ -120,6 +154,18 @@ class ArrayUtil {
 	}
 
 	/**
+	*/
+	macro public static function pluck<T,TOut>( arr : ExprOf<Array<T>>, expr : ExprOf<TOut> ) : ExprOf<Array<TOut>> {
+		return macro $e{arr}.map( function(_) return ${expr} );
+	}
+
+	/**
+	*/
+	public static inline function random<T>( arr : Array<T> ) : T {
+		return arr[ Math.floor( Math.random() * arr.length - 1 ) ];
+	}
+
+	/**
 		Resizes an array of `T` to an arbitrary length by adding more elements to its end or by removing extra elements.
 		Note that the function changes the passed array and doesn't create a copy.
 	**/
@@ -141,6 +187,14 @@ class ArrayUtil {
 	}
 
 	/**
+	*/
+	public static inline function reversed<T:Int>( a : Array<T> ) : Array<T> {
+		var b = a.copy();
+		b.reverse();
+		return b;
+	}
+
+	/**
 		Shuffles given array in place.
 	*/
 	public static function shuffle<T>( a : Array<T> ) : Array<T> {
@@ -158,12 +212,6 @@ class ArrayUtil {
 
 	/**
 	*/
-	public static inline function random<T>( arr : Array<T> ) : T {
-		return arr[Std.int(Math.random()*arr.length-1)];
-	}
-
-	/**
-	*/
 	public static function sorted<T>( a : Array<T>, f : T->T->Int ) : Array<T> {
 		var n = a.copy();
     	n.sort( f );
@@ -172,35 +220,13 @@ class ArrayUtil {
 
 	/**
 	*/
-	public static inline function reversed<T:Int>( a : Array<T> ) : Array<T> {
-		var b = a.copy();
-    	b.reverse();
-    	return b;
-	}
-
-	/**
-	*/
-	public static inline function dropLeft<T>( a: Array<T>, n : Int ) : Array<T> {
-		return (n >= a.length) ? [] : a.slice( n );
-	}
-
-	/**
-	*/
-	public static inline function dropRight<T>( a: Array<T>, n : Int ) : Array<T> {
-		return (n >= a.length) ? [] : a.slice( 0, a.length - n );
-	}
-
-	/**
-	*/
 	//public static inline function split<T:Int>( a : Array<T>, parts : Int ) : Array<T> {
 
-
-	////////////////////////////////////////////////////////////////////////////
-
 	/**
+		Returns a copy of the array with the new element added to the end.
 	*/
-	macro public static function pluck<T,TOut>( arr : ExprOf<Array<T>>, expr : ExprOf<TOut> ) : ExprOf<Array<TOut>> {
-		return macro $e{arr}.map( function(_) return ${expr} );
+	public inline static function with<T>( a : Array<T>, e : T ) : Array<T> {
+		return a.concat( [e] );
 	}
 
 }
