50d3c4f6b6d21b7ae8d61611171b59f99b9856cc
tong <tong@disktree.net>
2017-09-28 03:16:35 +0200

    Cleanup

--- src/om/macro/FieldTools.hx
+++ src/om/macro/FieldTools.hx
@@ -8,46 +8,34 @@ import haxe.macro.Type;
 
 class FieldTools {
 
-	public static function isPublic( field : Field ) : Bool {
-		for( access in field.access ) switch access {
-			case APublic: return true;
-			case _:
+	public static function appendExprToFunctionField( field : Field, expr : Expr ) {
+		switch field.kind {
+		case FFun(o):
+			var exprs = [o.expr,expr];
+			o.expr = macro $b{exprs};
+		case _:
 		}
-		return false;
 	}
 
-	public static function isStatic( field : Field ) : Bool {
-		for( access in field.access ) switch access {
-			case AStatic: return true;
-			case _:
-		}
-		return false;
-	}
-
-	public static function isMethod( field : Field ) : Bool {
-		return switch field.kind {
-			case FFun(_): true;
-			case _: false;
+	public static function createFunField( name : String, ?args : Array<FunctionArg>, ?ret : ComplexType, ?expr : Expr ) : Field {
+		return {
+			name: name,
+			access: [APublic],
+			kind: FFun({
+				ret  : (null != ret) ? ret : macro : Void,
+				expr : (null != expr) ? expr : macro {},
+				args : (null != args) ? args : []
+			}),
+			pos: Context.currentPos()
 		};
 	}
 
-	public static function isFieldInHierarchy( type : ClassType, name : String ) : Bool {
-		if( name == "new" ) {
-			if( type.constructor != null )
-				return true;
-		} else {
-			if( hasClassField( type.fields.get(), name ) )
-				return true;
-		}
-		var superClass = type.superClass;
-		if( superClass == null )
-			return false;
-		return isFieldInHierarchy( superClass.t.get(), name );
-	}
-
-	public static function findField( fields : Array<Field>, name : String ) : Field {
-		for( f in fields ) if( f.name == name ) return f;
-		return null;
+	public static function createVarField( name : String, type : ComplexType ) : Field {
+		return {
+			name: name,
+			kind: FVar(type,null),
+			pos: Context.currentPos()
+		};
 	}
 
 	public static function findClassField( fields : Array<ClassField>, name : String ) : ClassField {
@@ -55,74 +43,9 @@ class FieldTools {
 		return null;
 	}
 
-	public static inline function hasField( fields : Array<Field>, name : String ) : Bool
-		return findField( fields, name ) != null;
-
-	public static inline function hasClassField( fields : Array<ClassField>, name : String ) : Bool
-		return findClassField( fields, name ) != null;
-
-	public static function hasVarField( fields : Array<Field>, fieldName : String ) : Bool {
-		for( f in fields )
-			if( f.name == fieldName &&
-				switch f.kind {
-				case FVar(_,_): true;
-				case _: false;
-				})
-				return true;
-		    return false;
-		}
-
-	public static function hasMeta( field : Field, name : String ) : Bool {
-	    if( field.meta == null )
-			return false;
-	    for( m in field.meta )
-			if( m.name == name )
-				return true;
-	    return false;
-	}
-
-	public static function hasFunField( fields : Array<Field>, name : String ) : Bool {
-		for( field in fields )
-			if( field.name == name &&
-				switch field.kind {
-				case FFun(_): true;
-				case _: false;
-			})
-			return true;
-		return false;
-	}
-
-	public static function makePublic( fields : Array<Field>, name : String ) {
-		var f = findField( fields, name );
-		if( f == null )
-			return;
-    	makeFieldPublic( f );
-	}
-
-	public static function makeFieldPublic( field : Field ) : Field {
-	    if( isPublic( field ) )
-			return field;
-		field.access.push( APublic );
-		return field;
-	}
-
-	public static function makeVarsPublic( fields : Array<Field> ) {
-		fields.map( function(f) switch f.kind {
-			case FVar(_,_) if( !isPublic( f ) ): f.access.push( APublic );
-			case _:
-		});
-	}
-
-	public static function createVarField( name : String, type : ComplexType ) : Field {
-    	return {
-			name: name,
-			kind: FVar(type,null),
-			pos: Context.currentPos()
-		};
-	}
-
-	public static function hasFunctionArguments( field : Field ) : Bool {
-		return getFunctionArguments( field ).length > 0;
+	public static function findField( fields : Array<Field>, name : String ) : Field {
+		for( f in fields ) if( f.name == name ) return f;
+		return null;
 	}
 
 	public static function getFunctionArguments( field : Field ) : Array<FunctionArg> {
@@ -134,7 +57,7 @@ class FieldTools {
 
 	public static function getVarAsFunctionArgs( fields : Array<Field> ) : Array<FunctionArg> {
 		return cast fields
-			.map(function(f) return switch f.kind {
+			.map( function(f) return switch f.kind {
 				case FVar(t,_) if( !isStatic(f) ):
 					{ name : f.name, type : t, opt : null, value : null }
 				case _:
@@ -142,17 +65,116 @@ class FieldTools {
 		}).filter( function(f) return f != null );
 	}
 
-	public static function createFunctionField( name : String, ?args : Array<FunctionArg>, ?ret : ComplexType, ?expr : Expr ) : Field {
-    	return {
-			name: name,
-			access: [APublic],
-			kind: FFun({
-		        ret  : (null != ret) ? ret : macro : Void,
-		        expr : (null != expr) ? expr : macro {},
-		        args : (null != args) ? args : []
-			}),
-	      	pos: Context.currentPos()
-	    };
+	public static inline function hasClassField( fields : Array<ClassField>, name : String ) : Bool
+		return findClassField( fields, name ) != null;
+
+	public static inline function hasField( fields : Array<Field>, name : String ) : Bool
+		return findField( fields, name ) != null;
+
+	public static function hasFunArguments( field : Field ) : Bool
+		return getFunctionArguments( field ).length > 0;
+
+	public static function hasFunField( fields : Array<Field>, name : String ) : Bool {
+		for( f in fields ) {
+			if( f.name == name &&
+				switch f.kind {
+					case FFun(_): true;
+					case _: false;
+				})
+				return true;
+			}
+		return false;
+	}
+
+	public static function hasVarField( fields : Array<Field>, fieldName : String ) : Bool {
+		for( f in fields ) {
+			if( f.name == fieldName &&
+				switch f.kind {
+				case FVar(_,_): true;
+				case _: false;
+				})
+				return true;
+		}
+		return false;
+	}
+
+	public static function hasMeta( field : Field, name : String ) : Bool {
+		if( field.meta == null )
+			return false;
+		for( m in field.meta )
+			if( m.name == name )
+				return true;
+		return false;
+	}
+
+	public static function isFieldInHierarchy( type : ClassType, name : String ) : Bool {
+		if( name == "new" ) {
+			if( type.constructor != null )
+				return true;
+		} else {
+			if( hasClassField( type.fields.get(), name ) )
+				return true;
+		}
+		var sup = type.superClass;
+		if( sup == null )
+			return false;
+		return isFieldInHierarchy( sup.t.get(), name );
+	}
+
+	public static function isMethod( field : Field ) : Bool {
+		return switch field.kind {
+			case FFun(_): true;
+			case _: false;
+		};
+	}
+
+	public static function isPublic( field : Field ) : Bool {
+		for( a in field.access ) switch a {
+			case APublic: return true;
+			case _:
+		}
+		return false;
+	}
+
+	public static function isStatic( field : Field ) : Bool {
+		for( a in field.access ) switch a {
+			case AStatic: return true;
+			case _:
+		}
+		return false;
+	}
+
+	public static function makeFieldPublic( field : Field ) : Field {
+		if( isPublic( field ) )
+			return field;
+		field.access.push( APublic );
+		return field;
+	}
+
+	public static function makePublic( fields : Array<Field>, name : String ) {
+		var f = findField( fields, name );
+		if( f == null )
+			return;
+    	makeFieldPublic( f );
+	}
+
+	public static function makeVarsPublic( fields : Array<Field> ) {
+		fields.map( function(f) switch f.kind {
+			case FVar(_,_) if( !isPublic( f ) ): f.access.push( APublic );
+			case _:
+		});
+	}
+
+	public static function pushConstFloatVar( fields : Array<Field>, name : String, value : Float ) : Array<Field> {
+		return pushConstVar( fields, name, macro:Float, value );
+	}
+
+	public static function pushConstIntVar( fields : Array<Field>, name : String, value : Int ) : Array<Field> {
+		return pushConstVar( fields, name, macro:Int, value );
+	}
+
+	public static function pushConstStringVar( fields : Array<Field>, name : String, value : String ) : Array<Field> {
+		return pushConstVar( fields, name, macro:String, value );
 	}
 
 	public static function pushConstVar<T>( fields : Array<Field>, name : String, type : ComplexType, value : T ) : Array<Field> {
@@ -163,27 +185,6 @@ class FieldTools {
     		pos: Context.currentPos()
     	});
 		return fields;
-	}
-
-	public static function pushConstIntVar( fields : Array<Field>, name : String, value : Int ) : Array<Field> {
-		return pushConstVar( fields, name, macro:Int, value );
-	}
-
-	public static function pushConstFloatVar( fields : Array<Field>, name : String, value : Float ) : Array<Field> {
-		return pushConstVar( fields, name, macro:Float, value );
-	}
-
-	public static function pushConstStringVar( fields : Array<Field>, name : String, value : String ) : Array<Field> {
-		return pushConstVar( fields, name, macro:String, value );
-	}
-
-	public static function appendExprToFunctionField( field : Field, expr : Expr ) {
-		switch field.kind {
-		case FFun(o):
-			var exprs = [o.expr, expr];
-			o.expr = macro $b{exprs};
-		case _:
-		}
 	}
 
 }
--- src/om/macro/MacroTools.hx
+++ src/om/macro/MacroTools.hx
@@ -5,20 +5,24 @@ package om.macro;
 import haxe.macro.Context;
 import haxe.macro.Expr;
 import haxe.macro.Type;
+import sys.FileSystem;
 
 class MacroTools {
 
-	public static inline function toExpr( v : Dynamic ) : Expr
-        return Context.makeExpr( v, Context.currentPos() );
-
-	public static inline function here() : Position
-		return Context.currentPos();
-
-	public static inline function position( ?pos : Position ) : Position
-		return (pos == null) ? Context.currentPos() : pos;
-
-	public static inline function currentModulePath() : String
+	public static inline function getCurrentModulePath() : String
 		return Context.getPosInfos( (macro null).pos ).file;
+
+	public static function getFileInClassPath( file : String ) : String {
+		for( p in Context.getClassPath() ) {
+			var path = p + file;
+			if( FileSystem.exists( path ) && !FileSystem.isDirectory( path ) )
+				return path;
+		}
+		return null;
+	}
+
+	public static function getFullClassName( cls : ClassType ) : String
+		return ((cls.pack.length > 0) ? cls.pack.join(".") + "." : "") + cls.name;
 
 	public static inline function getLocalClass() : ClassType
 		return Context.getLocalClass().get();
@@ -26,8 +30,16 @@ class MacroTools {
 	public static inline function getLocalClassName() : String
 		return Context.getLocalClass().toString();
 
-	public static function getFullClassName( cls : ClassType ) : String
-		return ((cls.pack.length > 0) ? cls.pack.join(".") + "." : "") + cls.name;
+	public static function getMainClass() : String {
+		var args = Sys.args();
+		return switch [args.indexOf('-main'), args.indexOf('-x')] {
+			case [-1, -1]: null;
+			case [v, -1] | [_, v]: args[v+1];
+		}
+	}
+
+	public static function getModuleDirectory( type : String ) : String
+		return getModulePath( type ).split( "/" ).slice( 0, -1 ).join( "/" );
 
 	public static function getModulePath( type : String) : String {
     	return switch Context.getType( type ) {
@@ -36,25 +48,14 @@ class MacroTools {
 		};
 	}
 
-	public static function getModuleDirectory( type : String ) : String
-		return getModulePath( type ).split( "/" ).slice( 0, -1 ).join( "/" );
+	public static inline function here() : Position
+		return Context.currentPos();
 
-	public static function getFileInClassPath( file : String ) : String {
-		for( p in Context.getClassPath() ) {
-			var path = p + file;
-			if( sys.FileSystem.exists( path ) && !sys.FileSystem.isDirectory( path ) )
-				return path;
-	    }
-	    return null;
-	}
+	public static inline function position( ?pos : Position ) : Position
+		return (pos == null) ? Context.currentPos() : pos;
 
-	public static function getMainClass() : String {
-		var args = Sys.args();
-		return switch [args.indexOf('-main'), args.indexOf('-x')] {
-		    case [-1, -1]: null;
-		    case [v, -1] | [_, v]: args[v+1];
-		}
-	}
+	public static inline function toExpr( v : Dynamic ) : Expr
+		return Context.makeExpr( v, Context.currentPos() );
 
 }
 
