b52a774d64729d9cf1c6c909c88516b6da689c61
tong <tong@disktree.net>
2018-07-18 16:55:11 +0200

    Sort methods

--- src/om/Haxe.hx
+++ src/om/Haxe.hx
@@ -6,13 +6,15 @@ import haxe.macro.Compiler;
 import haxe.macro.Context;
 import haxe.macro.PositionTools.here;
 import om.macro.MacroTools.*;
-using StringTools;
+using haxe.macro.ExprTools;
+using om.StringTools;
 #end
 
-@:enum abstract CompilerTarget(String) from String to String {
+@:enum abstract CompilerTarget(String) to String {
     var cpp = 'cpp';
     var cs = 'cs';
     var hl = 'hl';
+    var hlc = 'hlc';
     var java = 'java';
     var js = 'js';
     var lua = 'lua';
@@ -22,6 +24,9 @@ using StringTools;
     var swf = 'swf';
 }
 
+/**
+    Compile time tools accessible from non-macro context.
+*/
 class Haxe {
 
     macro public static function addResource( name : String, data : Bytes ) {
@@ -38,18 +43,19 @@ class Haxe {
         return toExpr( Date.now().toString() );
     }
 
-    macro public static  function define( name : String, ?value : String ) {
-        Compiler.define( $v{name}, $v{value} );
-        return macro null;
-    }
+	macro public static  function define( name : String, ?value : String ) {
+		Compiler.define( $v{name}, $v{value} );
+		return macro null;
+	}
 
-    macro public static function defined( key : String ) : ExprOf<Bool> {
-        return macro $v{Context.defined( key )};
-    }
+	macro public static function defined( key : String ) : ExprOf<Bool> {
+		return macro $v{Context.defined( key )};
+	}
 
-    macro public static  function definedValue<T>( key : String, ?def : T ) : T {
-        return Context.defined( $v{key} ) ? cast Context.definedValue( $v{key} ) : $v{def};
-    }
+	macro public static function definedValue( key : String, ?def : String ) : ExprOf<String> {
+		var v = Context.defined( key ) ? Context.definedValue( key ) : def;
+		return macro $v{v};
+	}
 
     macro public static function error( msg : String ) : ExprOf<Date> {
         Context.error( msg, here() );
@@ -61,34 +67,35 @@ class Haxe {
         return macro null;
     }
 
-    macro public static function getClassPath() : ExprOf<Array<String>> {
-        return macro $v{Context.getClassPath()};
-    }
+	macro public static function getClassPath() : ExprOf<Array<String>> {
+		return macro $v{Context.getClassPath()};
+	}
 
-    macro public static function getCompilerTarget() : ExprOf<CompilerTarget> {
-        var t = om.macro.MacroTools.getCompilerTarget();
-        return macro $v{t};
-    }
+	macro public static function getCwd() : ExprOf<String> {
+		return macro $v{Sys.getCwd()};
+	}
 
-    macro public static function getCwd() : ExprOf<String> {
-        return macro $v{Sys.getCwd()};
-    }
+	macro public static function getDefines() : ExprOf<Array<Array<String>>> {
+		var map = Context.getDefines();
+		var arr = new Array<Array<String>>();
+		for( key in map.keys() ) {
+			arr.push( [key,Std.string(map.get(key))] );
+		}
+		//return macro $v{map};
+		return Context.makeExpr( arr, here() );
+	}
 
-    macro public static function getDefines() : ExprOf<Array<Array<String>>> {
-        var map = Context.getDefines();
-        var arr = new Array<Array<String>>();
-        for( key in map.keys() ) {
-            arr.push( [key,Std.string(map.get(key))] );
-        }
-        //return macro $v{map};
-        return Context.makeExpr( arr, here() );
-    }
+	macro public static function getCompilerTarget() : ExprOf<CompilerTarget> {
+		var t = om.macro.MacroTools.getCompilerTarget();
+		return macro $v{t};
+	}
 
-    macro public static function getOutput() : ExprOf<String> {
-        return macro $v{Compiler.getOutput()};
-    }
+	macro public static function getOutput() : ExprOf<String> {
+		return macro $v{Compiler.getOutput()};
+	}
 
-    macro public static function getTargetLineComment( ?target : String ) : ExprOf<String> {
+	/*
+	macro public static function getTargetLineComment( ?target : String ) : ExprOf<String> {
 		if( target == null ) target = om.macro.MacroTools.getCompilerTarget();
 		return macro $v{switch target {
 			case 'lua': '--';
@@ -96,24 +103,25 @@ class Haxe {
 			case _: '//';
 		}}
 	}
+	*/
 
-    macro public static function injectCode( code : String ) : ExprOf<String> {
-        var target = om.macro.MacroTools.getCompilerTarget();
-        return switch target {
-        case cpp: macro untyped __cpp__( '$code' );
-        case cs: macro untyped __cs__( '$code' );
-        case js: macro untyped __js__( '$code' );
-        case lua: macro untyped __lua__( '$code' );
-        case php: macro untyped __php__( '$code' );
-        case java: macro untyped __java__( '$code' );
-        //case 'python': macro untyped __python__( '$code' );
-        default:
-            Context.warning( 'injectCode not implemented [$target]', here() );
-            macro null;
-        }
-    }
+	macro public static function injectCode( code : String ) : ExprOf<String> {
+		var target = om.macro.MacroTools.getCompilerTarget();
+		return switch target {
+		case cpp: macro untyped __cpp__( '$code' );
+		case cs: macro untyped __cs__( '$code' );
+		case js: macro untyped __js__( '$code' );
+		case lua: macro untyped __lua__( '$code' );
+		case php: macro untyped __php__( '$code' );
+		case java: macro untyped __java__( '$code' );
+		//case 'python': macro untyped __python__( '$code' );
+		default:
+			Context.warning( 'injectCode not implemented [$target]', here() );
+			macro null;
+		}
+	}
 
-    //TODO injects a ; at end
+	//TODO injects a ; at end
     macro public static function injectComment( str : String ) {
         var target = om.macro.MacroTools.getCompilerTarget();
         var comment = getTargetLineComment();
@@ -132,27 +140,40 @@ class Haxe {
         }
     }
 
-    macro public static function isDebug() : ExprOf<Bool> {
-        return macro #if debug true #else false #end;
-    }
+	macro public static function isDebug() : ExprOf<Bool> {
+		return macro #if debug true #else false #end;
+	}
 
-    macro public static function now() : ExprOf<Date> {
-        var d = Date.now();
-        return macro new Date( $v{d.getFullYear()}, $v{d.getMonth()}, $v{d.getDate()}, $v{d.getHours()}, $v{d.getMinutes()}, $v{d.getSeconds()} );
-    }
+	macro public static function now() : ExprOf<Date> {
+		var d = Date.now();
+		return macro new Date( $v{d.getFullYear()}, $v{d.getMonth()}, $v{d.getDate()}, $v{d.getHours()}, $v{d.getMinutes()}, $v{d.getSeconds()} );
+	}
 
     macro public static function parseDefine( key : String ) {
         return Context.parse( Context.definedValue( key ), here() );
     }
 
-    macro public static function println( msg : String ) {
-        Sys.println( msg );
-        return macro null;
-    }
+	macro public static function print( msg : String ) {
+		Sys.print( msg );
+		return macro null;
+	}
 
-    macro public static function warn( msg : String ) : ExprOf<Date> {
-        Context.warning( msg, here() );
-        return macro null;
-    }
+	macro public static function println( msg : String ) {
+		Sys.println( msg );
+		return macro null;
+	}
+
+	/**
+		Haxe compiler version.
+	*/
+	macro public static function version() : ExprOf<String> {
+		var v = haxe.macro.Context.definedValue( 'haxe_ver' );
+		return macro $v{v};
+	}
+
+	macro public static function warn( msg : String ) : ExprOf<Date> {
+		Context.warning( msg, here() );
+		return macro null;
+	}
 
 }
