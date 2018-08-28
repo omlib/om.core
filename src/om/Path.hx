410609d13cbf6bb643d780cefb196aac3193fd47
tong <tong@disktree.net>
2018-08-28 13:26:51 +0200

    Cleanup and extra methods

--- src/om/Path.hx
+++ src/om/Path.hx
@@ -4,48 +4,44 @@ package om;
 	Abstract over `haxe.io.Path`
 */
 @:forward(backslash,dir,ext,file)
+@:forwardStatics(addTrailingSlash,directory,extension,isAbsolute,join,normalize,removeTrailingSlashes,withExtension,withoutDirectory,withoutExtension)
 abstract Path(haxe.io.Path) {
 
-    public inline function new( str : String ) this = new haxe.io.Path( str );
+	public static inline var SEPERATOR = '/';
+
+    public inline function new( str : String )
+		this = new haxe.io.Path( str );
 
 	//TODO remove empty parts ?
     @:to public inline function toArray() : Array<String>
-        return this.toString().split('/');
+        return this.toString().split( SEPERATOR );
 
     @:to public inline function toString() : String
         return this.toString();
 
-	public static function addTrailingSlash( path : String ) : String
-		return haxe.io.Path.addTrailingSlash( path );
+	@:from public static inline function fromArray( a : Array<String> ) : om.Path
+		return om.Path.join( a );
 
-	public static function directory( path : String ) : String
-		return haxe.io.Path.directory( path );
-
-	public static function extension( path : String ) : String
-		return haxe.io.Path.extension( path );
-
-	public static inline function isAbsolute( path : String ) : Bool
-		return haxe.io.Path.isAbsolute( path );
-
-	public static function join( paths : Array<String> ) : String
-		return haxe.io.Path.join( paths );
-
-	public static function normalize( path : String ) : String
-		return haxe.io.Path.normalize( path );
-
-	public static inline function removeTrailingSlashes( path : String ) : String
-		return haxe.io.Path.removeTrailingSlashes( path );
-
-	public static inline function withExtension( path : String, ext : String ) : String
-		return haxe.io.Path.withExtension( path, ext );
-
-	public static inline function withoutDirectory( path : String ) : String
-		return haxe.io.Path.withoutDirectory( path );
-
-	public static inline function withoutExtension( path : String ) : String
-		return haxe.io.Path.withoutExtension( path );
+	@:from public static inline function fromList( l : List<String> ) : om.Path
+		return fromString( om.Path.normalize( l.join( SEPERATOR ) ) );
 
 	@:from public static inline function fromString( s : String ) : om.Path
-		return new om.Path(s);
+		return new om.Path( s );
+
+	// --- Extra methods
+
+	public static function hasExtension( path : String, ?ext : String ) : Bool {
+		return (ext == null)
+			? Path.extension( path ).length > 0
+			: Path.extension( path ) == ext;
+	}
+
+	public static function replaceExtension( path : String, ?ext : String ) : String {
+		return switch ext {
+			case null: Path.withoutExtension( path );
+			case _ if( ext.length == 0 ): throw 'invalid extension';
+			default: Path.withoutExtension( path ) + '.$ext';
+		}
+	}
 
 }
