76a435a73a7b0a6472de537c8a4b94ae40c7979d
tong <tong@disktree.net>
2018-08-28 13:58:00 +0200

    Cleanup

--- src/om/Resource.hx
+++ src/om/Resource.hx
@@ -3,6 +3,20 @@ package om;
 @:forwardStatics
 abstract Resource(haxe.Resource) {
 
+	public static macro function all() : ExprOf<Map<String,haxe.io.Bytes>> {
+		var m = haxe.macro.Context.getResources();
+		return macro $a{ list().map( f -> {
+			var n = f;
+			var v = macro $v{m.get(f)};
+			return macro $v{n} => $v;
+		}) };
+	}
+
+	public static macro function set( name : String, str : String ) {
+		haxe.macro.Context.addResource( name, haxe.io.Bytes.ofString( str ) );
+		return macro null;
+	}
+
 	public static inline function get( name : String ) : String {
 		return std.haxe.Resource.getString( name );
 	}
@@ -15,25 +29,16 @@ abstract Resource(haxe.Resource) {
 		return std.haxe.Resource.listNames();
 	}
 
+	public static inline function count() : Int {
+		return list().length;
+	}
+
 	/*
 	#if php
 	@:access(haxe.Resource.getPath)
-	public static inline function getPath( name : String )  : String {
+	public static inline function getPath( name : String ) : String {
 		return std.haxe.Resource.getPath( name );
 	}
 	#end
 	*/
-
-	/*
-	macro public static function addString( name : String, content : String ) {
-		haxe.macro.Context.addResource( name, haxe.io.Bytes.ofString( content ) );
-		return macro null;
-	}
-
-	macro public static function addBytes( name : String, content : haxe.io.Bytes ) {
-		haxe.macro.Context.addResource( name, content );
-		return macro null;
-	}
-	*/
-
 }
