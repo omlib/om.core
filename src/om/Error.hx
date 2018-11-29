5897080607b20b64a06bebf131a5026d3d1028ea
tong <tong@disktree.net>
2018-11-29 14:01:22 +0100

    ErrorType base class

--- src/om/Error.hx
+++ src/om/Error.hx
@@ -2,60 +2,18 @@ package om;
 
 import haxe.PosInfos;
 import haxe.CallStack;
+import om.error.ErrorType;
 
-class Error #if js extends js.Error #end {
+@:forward(message,pos,stackItems,getCallStack,getExceptionStack,getSourcePosition,stackToString)
+abstract Error(ErrorType) {
 
-	public var pos(default,null) : PosInfos;
+	@:from public inline static function fromString( str : String ) : Error
+		return new Error( str );
 
-	#if !js
-    public var message(default,null) : String;
-    public var name(default,null) : String;
-    #end
+	public inline function new( ?message : String, ?stack : Array<StackItem>, ?pos : PosInfos )
+		this = new ErrorType( message, stack, pos );
 
-	public var fileName(get,never) : String;
-	inline function get_fileName() return pos.fileName;
-
-	public var lineNumber(get,never) : Int;
-	inline function get_lineNumber() return pos.lineNumber;
-
-	public var methodName(get,never) : String;
-	inline function get_methodName() return pos.methodName;
-
-	public var stackItems(default,null) : Array<StackItem>;
-
-	public function new( ?message : String, ?stack : Array<StackItem>, ?pos : PosInfos ) {
-
-		#if js
-		super( message );
-		#end
-		this.message = message;
-
-		var cl = Type.getClassName( Type.getClass( this ) );
-		if( cl.indexOf( '.' ) != -1 ) cl = cl.split('.').pop();
-		this.name = cl;
-		this.pos = pos;
-
-		if( stack == null ) {
-			stack = try CallStack.exceptionStack() catch(e:Dynamic) [];
-			if( stack.length == 0 )
-				stack = try CallStack.callStack() catch(e:Dynamic) [];
-		}
-		this.stackItems = stack;
-	}
-
-	public inline function getCallStack() : Array<StackItem>
-		return CallStack.callStack();
-
-	public inline function getExceptionStack() : Array<StackItem>
-		return CallStack.exceptionStack();
-
-	public inline function getSourcePosition() : String
-		return pos.fileName + ':' + pos.lineNumber;
-
-	public function toString() : String {
-		var items = getExceptionStack();
-		if( items.length == 0 ) items = getCallStack();
-		return getSourcePosition() + ' : ' + message + CallStack.toString( items );
-	}
+	@:to public inline function toString() : String
+		return this.toString();
 
 }
---
+++ src/om/error/ErrorType.hx
@@ -0,0 +1,62 @@
+package om.error;
+
+import haxe.PosInfos;
+import haxe.CallStack;
+
+class ErrorType #if js extends js.Error #end {
+
+	#if !js
+    public var message(default,null) : String;
+    public var name(default,null) : String;
+    #end
+
+	public var pos(default,null) : PosInfos;
+	public var stackItems(default,null) : Array<StackItem>;
+
+	public var fileName(get,never) : String;
+	inline function get_fileName() return pos.fileName;
+
+	public var lineNumber(get,never) : Int;
+	inline function get_lineNumber() return pos.lineNumber;
+
+	public var methodName(get,never) : String;
+	inline function get_methodName() return pos.methodName;
+
+    public function new( ?message : String, ?stack : Array<StackItem>, ?pos : PosInfos ) {
+
+		#if js
+		super( message );
+		#end
+		this.message = message;
+
+		var cl = Type.getClassName( Type.getClass( this ) );
+		if( cl.indexOf( '.' ) != -1 ) cl = cl.split('.').pop();
+		this.name = cl;
+		this.pos = pos;
+
+		if( stack == null ) {
+			stack = try CallStack.exceptionStack() catch(e:Dynamic) [];
+			if( stack.length == 0 )
+				stack = try CallStack.callStack() catch(e:Dynamic) [];
+		}
+		this.stackItems = stack;
+	}
+
+	public inline function getCallStack() : Array<StackItem>
+		return CallStack.callStack();
+
+	public inline function getExceptionStack() : Array<StackItem>
+		return CallStack.exceptionStack();
+
+	public inline function getSourcePosition() : String
+		return pos.fileName + ':' + pos.lineNumber;
+
+	public inline function stackToString() : String
+		return CallStack.toString( stackItems );
+
+	public function toString() : String {
+		var s = getSourcePosition();
+		if( message != null && message.length > 0 ) s += ' : $message';
+		return s + stackToString();
+	}
+}
