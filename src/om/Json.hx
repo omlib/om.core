8730cb3081325098ea329df1728661db4fe9f593
tong <tong@disktree.net>
2018-12-23 14:29:47 +0100

    Shortcuts for sys targets

--- src/om/Json.hx
+++ src/om/Json.hx
@@ -1,3 +1,20 @@
 package om;
 
-typedef Json = haxe.Json;
+class Json {
+
+	public static inline function parse( str : String ) : Dynamic {
+		return haxe.Json.parse( str );
+	}
+
+	public static inline function stringify( v : Dynamic, ?replacer : (key:Dynamic,value:Dynamic)->Dynamic, ?space : String  ) : String {
+		return haxe.Json.stringify( v, replacer, space );
+	}
+
+	#if (sys||nodejs)
+
+	public static inline function parseFile( path : String ) : Dynamic {
+		return haxe.Json.parse( sys.io.File.getContent( path ) );
+	}
+
+	#end
+}
