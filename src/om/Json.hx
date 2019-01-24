e61a2970463b91a323eaf3bf4e856d8b54c41077
tong <tong@disktree.net>
2019-01-24 09:41:13 +0100

    Json.writeFile promise method

--- src/om/Json.hx
+++ src/om/Json.hx
@@ -6,7 +6,7 @@ class Json {
 		return haxe.Json.parse( str );
 	}
 
-	public static inline function stringify( v : Dynamic, ?replacer : (key:Dynamic,value:Dynamic)->Dynamic, ?space : String  ) : String {
+	public static inline function stringify( v : Dynamic, ?replacer : (key:Dynamic,value:Dynamic)->Dynamic, ?space : String ) : String {
 		return haxe.Json.stringify( v, replacer, space );
 	}
 
@@ -26,10 +26,26 @@ class Json {
 		});
 	}
 
+	public static function writeFile( v : Dynamic, path : String, ?options : haxe.extern.EitherType<String,js.node.Fs.FsWriteFileOptions>, ?replacer : (key:Dynamic,value:Dynamic)->Dynamic, ?space : String ) : Promise<Nil> {
+		return new Promise( (resolve,reject) -> {
+			js.node.Fs.writeFile( path, stringify( v, replacer, space ), options, e -> (e != null) ? reject( e ) : resolve( om.Nil.nil ) );
+		});
+	}
+
+	#elseif js
+
+	public static inline function fetch<T>( input : haxe.extern.EitherType<js.html.Request,String>, ?init : js.html.RequestInit ) : Promise<T> {
+        return FetchTools.fetchJson( input, init );
+    }
+
 	#elseif sys
 
-	public static inline function readFile( path : String ) : Dynamic {
+	public static inline function readFile<T>( path : String ) : T {
 		return parse( sys.io.File.getContent( path ) );
+	}
+
+	public static inline function writeFile( v : Dynamic, path : String, ?replacer : (key:Dynamic,value:Dynamic)->Dynamic, ?space : String ) {
+		sys.io.File.saveContent( stringify( v, replacer, space ), path );
 	}
 
 	#end
