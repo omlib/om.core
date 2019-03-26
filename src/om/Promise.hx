54750b6446d57445c60fbb46442d0370e6075d55
tong <tong@disktree.net>
2019-03-26 10:22:19 +0100

    Promise helper methods

--- src/om/Promise.hx
+++ src/om/Promise.hx
@@ -1,5 +1,28 @@
 package om;
 
 #if js
-typedef Promise<T> = js.Promise<T>;
+
+import js.Promise;
+
+@:forward
+@:forwardStatics
+abstract Promise<T>(js.Promise<T>) from js.Promise<T> to js.Promise<T> {
+
+	public inline function new( init : (resolve:(value:T)->Void, reject:(reason:Dynamic)->Void)->Void )
+		this = new js.Promise( init );
+	
+	/**
+	**/
+	public static inline function nil() : Promise<Nil>
+		return Promise.resolve( om.Nil.nil );
+
+	/**
+		Delayed resolve.
+	**/
+	public static inline function delay<T>( ms : Int, ?v : T ) : om.Promise<T> {
+		return new Promise( (res,_) -> haxe.Timer.delay( () -> res(v), ms ) );
+	}
+
+}
+
 #end
