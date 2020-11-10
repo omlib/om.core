3e54d741366229d05942b93bc4d5bef54d76869f
tong <tong@disktree.net>
2020-11-10 19:35:51 +0100

    More Browser helper methods, cleanup

--- src/om/Browser.hx
+++ src/om/Browser.hx
@@ -2,37 +2,36 @@ package om;
 
 #if js
 
+using om.Path;
+
 class Browser {
 
 	public static var supported(get, never):Bool;
 	extern static inline function get_supported() return js.Syntax.typeof(window) != "undefined";
 
-	public static var window(get,never) : js.html.Window;
-	extern static inline function get_window() return untyped __js__("window");
-	
-	public static var document(get,never) : js.html.HTMLDocument;
-	extern static inline function get_document() return window.document;
-	
-	public static var navigator(get,never) : js.html.Navigator;
-	extern static inline function get_navigator() return window.navigator;
-	
 	public static var console(get,never) : js.html.ConsoleInstance;
 	extern static inline function get_console() return window.console;
-	
+
+	public static var crypto(get,never) : js.html.Crypto;
+	extern static inline function get_crypto() return window.crypto;
+
+	public static var document(get,never) : js.html.HTMLDocument;
+	extern static inline function get_document() return window.document;
+
+	public static var navigator(get,never) : js.html.Navigator;
+	extern static inline function get_navigator() return window.navigator;
+
 	public static var history(get,never) : js.html.History;
 	extern static inline function get_history() return window.history;
 
 	public static var localStorage(get,never) : js.html.Storage;
 	extern static inline function get_localStorage() return window.localStorage;
 
-	public static var sessionStorage(get,never) : js.html.Storage;
-	extern static inline function get_sessionStorage() return window.sessionStorage;
-
 	public static var location(get,never) : js.html.Location;
 	extern static inline function get_location() return window.location;
 
-	public static var crypto(get,never) : js.html.Crypto;
-	extern static inline function get_crypto() return window.crypto;
+	public static var sessionStorage(get,never) : js.html.Storage;
+	extern static inline function get_sessionStorage() return window.sessionStorage;
 
 	public static var orientation(get,never) : Int;
 	extern static inline function get_orientation() return window.orientation;
@@ -45,7 +44,10 @@ class Browser {
 
 	public static var speechSynthesis(get,never) : js.html.SpeechSynthesis;
 	extern static inline function get_speechSynthesis() return window.speechSynthesis;
-	
+
+	public static var window(get,never) : js.html.Window;
+	extern static inline function get_window() return js.Syntax.code("window");
+
 	public static inline function print() window.print();
 
 	public static inline function alert( v : Dynamic )
@@ -74,7 +76,28 @@ class Browser {
 		a.click();
 		URL.revokeObjectURL( url );
 	}
-	
+
+	public static function saveTextFile( name : String, str : String ) {
+		//var url = URL.createObjectURL( new js.html.Blob( [new js.lib.DataView(data)], { type: type } ) );
+		var a = document.createAnchorElement();
+		a.href = 'data:text/plain;charset=utf-8,' + StringTools.urlEncode( str );
+		//a.href = 'data:text/plain' + StringTools.urlEncode( str );
+		a.download = name;
+		a.style.display = 'none';
+		document.body.appendChild( a );
+		a.click();
+		document.body.removeChild( a );
+	}
+
+	public static function doClick( href : String ) {
+		var a = document.createAnchorElement();
+		a.href = href;
+		a.style.display = 'none';
+		document.body.appendChild( a );
+		a.click();
+		document.body.removeChild( a );
+	}
+
 }
 
 #end
