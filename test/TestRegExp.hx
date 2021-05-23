50f44e2204a281a569f1fecbf0039a825aa3d621
tong <tong@disktree.net>
2021-05-23 06:02:03 +0200

    Refine unit test cases

---
+++ test/TestRegExp.hx
@@ -0,0 +1,18 @@
+
+import om.RegExp;
+import utest.Assert.*;
+
+class TestRegExp extends utest.Test {
+
+	function test_construct() {
+        
+        var e = new RegExp("(haxe)");
+		isTrue( e.match("haxe") );
+		equals( "haxe", e.matched(1) );
+        
+        var e : RegExp = "(haxe)";
+		isTrue( e.match("haxe") );
+		equals( "haxe", e.matched(1) );
+	}
+
+}
--- test/Unit.hx
+++ test/Unit.hx
@@ -2,63 +2,56 @@
 import utest.Runner;
 import utest.ui.Report;
 
-class Unit {
-
-	static function main() {
-
-		var runner = new Runner();
-
-		//runner.addCase( new TestLazy() );
-		//runner.addCase( new TestURL() );
-
-		runner.addCase( new TestAbstractEnumTools() );
-		runner.addCase( new TestArrayBufferTools() );
-		runner.addCase( new TestArrayTools() );
-		runner.addCase( new TestClock() );
-		runner.addCase( new TestEmitter() );
-		runner.addCase( new TestErrors() );
-		runner.addCase( new TestFloatTools() );
-		runner.addCase( new TestIntTools() );
-		runner.addCase( new TestJson() );
-		runner.addCase( new TestMath() );
-		runner.addCase( new TestMaybe() );
-		runner.addCase( new TestNil() );
-		runner.addCase( new TestPath() );
-		//runner.addCase( new TestPromise() );
-		//runner.addCase( new TestRandom() );
-		//runner.addCase( new TestResource() );
-		runner.addCase( new TestSemVer() );
-		runner.addCase( new TestStringTools() );
-		runner.addCase( new TestSystem() );
-		runner.addCase( new TestTime() );
-		runner.addCase( new TestWorker() );
- 
+function main() {
+	var runner = new Runner();
+	runner.addCase( new TestAbstractEnumTools() );
+	runner.addCase( new TestArrayBufferTools() );
+	runner.addCase( new TestArrayTools() );
+	//runner.addCase( new TestChar() );
+	runner.addCase( new TestClock() );
+	runner.addCase( new TestEmitter() );
+	runner.addCase( new TestErrors() );
+	//runner.addCase( new TestFloatTools() );
+	runner.addCase( new TestIntTools() );
+	runner.addCase( new TestJson() );
+	runner.addCase( new TestMath() );
+	runner.addCase( new TestMaybe() );
+	runner.addCase( new TestNil() );
+	//runner.addCase( new TestPair() );
+	runner.addCase( new TestPath() );
+	//runner.addCase( new TestPromise() );
+	//runner.addCase( new TestRandom() );
+	runner.addCase( new TestRegExp() );
+	//runner.addCase( new TestResource() );
+	//runner.addCase( new TestResult() );
+	runner.addCase( new TestSemVer() );
+	runner.addCase( new TestStringTools() );
+	runner.addCase( new TestSystem() );
+	runner.addCase( new TestTime() );
+	//runner.addCase( new TestTuple() );
+	runner.addCase( new TestWorker() );
+	//runner.addCase( new TestUuid() );
+	#if js
+	//runner.addCase( new TestWorker() );
+	#end
+	var report = Report.create( runner );
+	/*
+	var report =
 		#if js
-		//runner.addCase( new TestWorker() );
-		#end
-
-		var report = Report.create( runner );
-
-		/*
-		var report =
-			#if js
-			if( om.System.hasWindow() ) {
-				new utest.ui.text.HtmlReport( runner, true );
-			} else {
-				Report.create( runner );
-			}
-			#else
+		if( om.System.hasWindow() ) {
+			new utest.ui.text.HtmlReport( runner, true );
+		} else {
 			Report.create( runner );
-			#end
-			*/
-
-		/*
-		report.setHandler(function(e){
-			trace(e);
-		});
+		}
+		#else
+		Report.create( runner );
+		#end
 		*/
 
-    	runner.run();
-	}
-
+	/*
+	report.setHandler(function(e){
+		trace(e);
+	});
+	*/
+	runner.run();
 }
