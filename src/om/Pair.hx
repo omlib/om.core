d30f6e6d0f3e49a7365b54f3e6226d6d236045a7
tong <tong@disktree.net>
2023-06-19 19:52:00 +0200

    Add Pair

---
+++ src/om/Pair.hx
@@ -0,0 +1,31 @@
+package om;
+
+@:pure
+abstract Pair<A,B>(CPair<A,B>) {
+
+    public var a(get,never) : A;
+    inline function get_a() : A return this.a;
+
+    public var b(get,never) : B;
+    inline function get_b() : B return this.b;
+
+    public inline function new(a : A, b : B) this = new CPair(a,b);
+
+    @:to inline function toArray() : Array<Dynamic>
+        return [this.a,this.b];
+
+    @:to inline function toBool() : Bool
+        return this != null;
+
+    @:op(!a) inline function isNull() : Bool
+        return this == null;
+}
+
+private class CPair<A,B> {
+    public var a : A;
+    public var b : B;
+    public inline function new(a : A, b : B) {
+        this.a = a;
+        this.b = b;
+    }
+}
---
+++ test/TestPair.hx
@@ -0,0 +1,30 @@
+import utest.Assert.*;
+import om.Pair;
+
+class TestPair extends utest.Test {
+
+	function test_pair() {
+		var pair = new Pair( "disktree", 23 );
+		equals( 'disktree', pair.a );
+		equals( 23, pair.b );
+    }
+	
+    function test_toArray() {
+        var a : Array<Dynamic> = new Pair( "disktree", 23 );
+		equals( "disktree", a[0] );
+		equals( 23, a[1] );
+    }
+
+   function test_toBool() {
+        var a : Bool = new Pair( "disktree", 23 );
+        isTrue(a);
+    }
+
+    function test_isNull() {
+        var p : Pair<String,Int> = null;
+        isNull( p );
+        p = new Pair( "disktree", 23 );
+        notNull( p );
+        isTrue( p );
+    } 
+}
