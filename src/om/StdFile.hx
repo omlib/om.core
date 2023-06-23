bf51a675df1e4aa148895daad269e27cceb59775
tong <tong@disktree.net>
2023-06-23 13:27:37 +0200

    Add StdFile enum

---
+++ src/om/StdFile.hx
@@ -0,0 +1,22 @@
+package om;
+
+/**
+  Input and output communication channels between a computer program and its environment when it begins execution.
+**/
+enum StdFile {
+
+    /**
+      Standard input stream (`stdin`).
+    **/
+    StdIn;
+
+    /**
+      Standard output stream (`sttdout`).
+    **/
+    StdOut;
+
+    /**
+      Standard error stream (`stderr`).
+    **/
+    StdErr;
+}
