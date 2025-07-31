6055b3632cff83eb306e0cd0297019aa953c25e3
tong <tong@disktree.net>
2025-07-31 16:02:51 +0200

    cleanup, some more FloatTools

--- src/om/FloatTools.hx
+++ src/om/FloatTools.hx
@@ -1,65 +1,103 @@
 package om;
 
+using StringTools;
+
 class FloatTools {
+	/**
+		Constant value employed to see if two `Float` values are very close.
+	**/
+	public static inline var EPSILON:Float = 1e-9;
 
-    static var pattern_parse = ~/^(\+|-)?(:?\d+(\.\d+)?(e-?\d+)?|nan|NaN|NAN)$/;
-    static var pattern_inf = ~/^\+?(inf|Inf|INF)$/;
-    static var pattern_neg_inf = ~/^-(inf|Inf|INF)$/;
+	public static var pattern_parse = ~/^(\+|-)?(:?\d+(\.\d+)?(e-?\d+)?|nan|NaN|NAN)$/;
+	public static var pattern_inf = ~/^\+?(inf|Inf|INF)$/;
+	public static var pattern_neg_inf = ~/^-(inf|Inf|INF)$/;
 
-    public static function canParse( s : String ) : Bool {
-        return pattern_parse.match(s) || pattern_inf.match(s) || pattern_neg_inf.match(s);
+	/**
+		Returns the shortest angular difference between two angles in the range (-turn/2, +turn/2].
+		Default turn is 360 (degrees).
+	**/
+	public static function angleDifference(a:Float, b:Float, ?turn:Float = 360.0) {
+		var r = (b - a) % turn;
+		if (r < 0)
+			r += turn;
+		if (r > turn / 2)
+			r -= turn;
+		return r;
 	}
 
-    public static function parse( s : String ) : Float {
-        if( s.substring( 0, 1 ) == "+" ) s = s.substring( 1 );
-        return if( pattern_inf.match( s ) ) Math.POSITIVE_INFINITY
-        else if( pattern_neg_inf.match( s ) ) Math.NEGATIVE_INFINITY
-        else Std.parseFloat( s );
-    }
+	/**
+		Returns `true` if a string value can be safely converted to`Float`.
+	**/
+	public static inline function canParse(s:String):Bool
+		return pattern_parse.match(s) || pattern_inf.match(s) || pattern_neg_inf.match(s);
+
+	/**
+		Rounds a number up to the specified number of decimals.
+	**/
+	public static function ceilTo(f:Float, decimals:Int):Float {
+		final p = Math.pow(10, decimals);
+		return Math.fceil(f * p) / p;
+	}
+
+	/**
+		Restricts a value within the specified range.
+	**/
+	public static inline function clamp(v:Float, min:Float, max:Float):Float
+		return v < min ? min : (v > max ? max : v);
+
+	/**
+		Returns the comparison value (int) between two `float` values.
+	**/
+	public static inline function compare(a:Float, b:Float):Int
+		return a < b ? -1 : (a > b ? 1 : 0);
+
+	/**
+		`Returns a value between `a` and `b` for any value of `f` between 0 and 1.
+	**/
+	public static function interpolate(f:Float, a:Float, b:Float):Float
+		return (b - a) * f + a;
+
+	/**
+		Checks if two angles are nearly equal, considering wrap-around.
+	**/
+	public static inline function nearEqualAngles(a:Float, b:Float, ?turn = 360.0, ?tollerance = Math.EPSILON):Bool
+		return Math.abs(angleDifference(a, b, turn)) <= tollerance;
+
+	/**
+		Parses a float from a string.
+		Recognizes optional '+' prefix, NaN, and infinities.
+	**/
+	public static function parse(s:String):Float {
+		if (s.substring(0, 1) == "+")
+			s = s.substring(1);
+		return if (pattern_inf.match(s)) Math.POSITIVE_INFINITY else if (pattern_neg_inf.match(s)) Math.NEGATIVE_INFINITY else Std.parseFloat(s);
+	}
+
+	/**
+		Rounds a float to the given number of decimal places.
+	**/
+	public static inline function precision(v:Float, digits:Int):Float {
+		final factor = Math.pow(10, Math.min(Math.max(digits, 0), 15));
+		return Math.round(v * factor) / factor;
+	}
+
+	/**
+		Formats a float to a string with exactly the given number of decimal places, preserving trailing zeroes.
+	**/
+	public static inline function precisionString(v:Float, digits:Int):String {
+		final parts = Std.string(precision(v, digits)).split(".");
+		final whole = parts[0];
+		final frac = parts.length > 1 ? parts[1] : "";
+		final paddedFrac = frac.lpad("0", digits);
+		return digits > 0 ? '$whole.$paddedFrac' : whole;
+	}
 
 	/**
 		Returns `-1` if `value` is a negative number, `1` otherwise.
-	*/
-	public static inline function sign<T:Float>( v : T ) : Int {
+	**/
+	public static inline function sign<T:Float>(v:T):Int
 		return v < 0 ? -1 : 1;
-	}
 
-    #if js
-
-
-    /**
-        //TODO https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/toPrecision
-        @param v
-        @param precision  Number of significant digits
-    */
-    /*
-	public static inline function precision( v : Float, precision : Int ) : Float {
-        return untyped __js__( "Number.parseFloat({0}).toPrecision({1})", v, precision );
-	}
-    */
-
-    #end
-
-    //TODO https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/toFixed
-
-    public static function precision( v : Float, precision : Int ) : Float {
-        //TODO !js test fails
-        //v *= Math.pow( 10, precision );
-        //precision += 1;
-        return Math.round( v * Math.pow( 10, precision ) ) / Math.pow( 10, precision );
-    }
-
-	public static inline function nearEqualAngles( a : Float, b : Float, ?turn = 360.0, ?tollerance = Math.EPSILON ) {
-		return Math.abs( angleDifference( a, b, turn ) ) <= tollerance;
-	}
-
-	public static function angleDifference(a : Float, b : Float, ?turn : Float = 360.0) {
-    var r = (b - a) % turn;
-    if(r < 0)
-      r += turn;
-    if(r > turn / 2)
-      r -= turn;
-    return r;
-  }
-
+	public static inline function toString(v:Float):String
+		return '$v';
 }
--- src/om/StringTools.hx
+++ src/om/StringTools.hx
@@ -3,171 +3,204 @@ package om;
 using om.ArrayTools;
 
 class StringTools {
+	public static inline function contains(s:String, value:String):Bool
+		return std.StringTools.contains(s, value);
 
-	public static inline function htmlEscape( s : String, ?quotes : Bool ) : String return std.StringTools.htmlEscape( s, quotes );
-	public static inline function htmlUnescape( s : String ) : String return std.StringTools.htmlUnescape( s );
-	//public static inline function postProcessUrlEncode( s : String ) : String return std.StringTools.postProcessUrlEncode( s );
-	public static inline function urlEncode( s : String ) : String return std.StringTools.urlEncode( s );
-	public static inline function urlDecode( s : String ) : String return std.StringTools.urlDecode( s );
-	public static inline function startsWith( s : String, start : String ) : Bool return std.StringTools.startsWith( s, start );
-	public static inline function endsWith( s : String, end : String ) : Bool return std.StringTools.endsWith( s, end );
-	public static inline function isSpace( s : String, pos : Int ) : Bool return std.StringTools.isSpace( s, pos );
-	public static inline function ltrim( s : String ) : String return std.StringTools.ltrim( s );
-	public static inline function rtrim( s : String ) : String return std.StringTools.rtrim( s );
-	public static inline function trim( s : String ) : String return std.StringTools.trim( s );
-	public static inline function lpad( s : String, c : String, l : Int ) : String return std.StringTools.lpad( s, c, l );
-	public static inline function rpad( s : String, c : String, l : Int ) : String return std.StringTools.rpad( s, c, l );
-	public static inline function replace( s : String, sub : String, by : String ) : String return std.StringTools.replace( s, sub, by );
-	public static inline function hex( n : Int, ?digits : Int ) : String return std.StringTools.hex( n, digits );
-	public static inline function fastCodeAt( s : String, index : Int ) : Int return std.StringTools.fastCodeAt( s, index );
-	public static inline function isEof( c : Int ) : Bool return std.StringTools.isEof( c );
-	public static inline function quoteUnixArg( argument : String ) : String return haxe.SysTools.quoteUnixArg( argument );
-	public static inline function quoteWinArg( argument : String, escapeMetaCharacters : Bool ) : String return haxe.SysTools.quoteWinArg( argument, escapeMetaCharacters );
+	public static inline function htmlEscape(s:String, ?quotes:Bool):String
+		return std.StringTools.htmlEscape(s, quotes);
 
-	//public static var SPLIT_LINES = ~/\r\n|\n\r|\n|\r/g;
+	public static inline function htmlUnescape(s:String):String
+		return std.StringTools.htmlUnescape(s);
 
-	public static function capitalize( str : String ) : String {
-		return str.charAt( 0 ).toUpperCase() + str.substr( 1 );
-		//return ~/\S/.replace( str, "$1".toUpperCase() );
+	// public static inline function postProcessUrlEncode( s : String ) : String return std.StringTools.postProcessUrlEncode( s );
+	public static inline function urlEncode(s:String):String
+		return std.StringTools.urlEncode(s);
+
+	public static inline function urlDecode(s:String):String
+		return std.StringTools.urlDecode(s);
+
+	public static inline function startsWith(s:String, start:String):Bool
+		return std.StringTools.startsWith(s, start);
+
+	public static inline function endsWith(s:String, end:String):Bool
+		return std.StringTools.endsWith(s, end);
+
+	public static inline function isSpace(s:String, pos:Int):Bool
+		return std.StringTools.isSpace(s, pos);
+
+	public static inline function ltrim(s:String):String
+		return std.StringTools.ltrim(s);
+
+	public static inline function rtrim(s:String):String
+		return std.StringTools.rtrim(s);
+
+	public static inline function trim(s:String):String
+		return std.StringTools.trim(s);
+
+	public static inline function lpad(s:String, c:String, l:Int):String
+		return std.StringTools.lpad(s, c, l);
+
+	public static inline function rpad(s:String, c:String, l:Int):String
+		return std.StringTools.rpad(s, c, l);
+
+	public static inline function replace(s:String, sub:String, by:String):String
+		return std.StringTools.replace(s, sub, by);
+
+	public static inline function hex(n:Int, ?digits:Int):String
+		return std.StringTools.hex(n, digits);
+
+	public static inline function fastCodeAt(s:String, index:Int):Int
+		return std.StringTools.fastCodeAt(s, index);
+
+	public static inline function isEof(c:Int):Bool
+		return std.StringTools.isEof(c);
+
+	public static inline function quoteUnixArg(argument:String):String
+		return haxe.SysTools.quoteUnixArg(argument);
+
+	public static inline function quoteWinArg(argument:String, escapeMetaCharacters:Bool):String
+		return haxe.SysTools.quoteWinArg(argument, escapeMetaCharacters);
+
+	// public static var SPLIT_LINES = ~/\r\n|\n\r|\n|\r/g;
+
+	public static function capitalize(str:String):String {
+		return str.charAt(0).toUpperCase() + str.substr(1);
+		// return ~/\S/.replace( str, "$1".toUpperCase() );
 	}
 
 	/**
 		Returns `true` if `src` contains one or more occurrences of `str`.
-	*/
-	public static inline function contains( src : String, str : String ) : Bool {
-		#if php
-		return str == "" || src.indexOf( str ) >= 0;
-		#else
-		return src.indexOf( str ) >= 0;
-		#end
+	 */
+	// public static inline function contains(src:String, str:String):Bool {
+	//	#if php
+	//	return str == "" || src.indexOf(str) >= 0;
+	//	#else
+	//	return src.indexOf(str) >= 0;
+	//	#end
+	// }
+
+	public static function containsAny(src:String, str:Array<String>):Bool {
+		return str.any(contains.bind(src, _));
 	}
 
-	public static function containsAny( src : String, str : Array<String> ) : Bool {
-		return str.any( contains.bind( src, _ ) );
+	public static inline function count(str:String, seperator = ''):Int {
+		return str.split(seperator).length;
 	}
 
-	public static inline function count( str : String, seperator = '' ) : Int {
-		return str.split( seperator ).length;
+	public static inline function countLines(str:String):Int {
+		return count(str, '\n');
 	}
 
-	public static inline function countLines( str : String ) : Int {
-		return count( str, '\n' );
-	}
-
-	public static function fromIntArray( a : Array<Int> ) : String {
+	public static function fromIntArray(a:Array<Int>):String {
 		var r = [];
-		for( i in 0...a.length ) {
-	        var c = a[i];
-	        if( c > 255 ) c &= 255;
-	        r.push( String.fromCharCode( c ) );
-	    }
-	    return r.join( "" );
+		for (i in 0...a.length) {
+			var c = a[i];
+			if (c > 255)
+				c &= 255;
+			r.push(String.fromCharCode(c));
+		}
+		return r.join("");
 	}
 
-	public static inline function hasUpperCase( s : String ) : Bool {
+	public static inline function hasUpperCase(s:String):Bool {
 		return s != s.toLowerCase();
 	}
 
-	public static inline function isEmpty( str : String ) : Bool {
+	public static inline function isEmpty(str:String):Bool {
 		return str == null || str == '';
 	}
 
-	public static inline function isLowerCase( str : String ) : Bool {
+	public static inline function isLowerCase(str:String):Bool {
 		return str.toLowerCase() == str;
 	}
 
-	public static inline function isUpperCase( str : String ) : Bool {
+	public static inline function isUpperCase(str:String):Bool {
 		return str.toUpperCase() == str;
 	}
 
-	public static inline function lines( str : String ) : Array<String> {
-		//return SPLIT_LINES.split( str );
-		return ~/\r\n|\n\r|\n|\r/g.split( str );
+	public static inline function lines(str:String):Array<String> {
+		// return SPLIT_LINES.split( str );
+		return ~/\r\n|\n\r|\n|\r/g.split(str);
 	}
 
-	public static inline function map<T>( str : String, cb : String->T ) : Array<T> {
-		return toArray( str ).map( cb );
+	public static inline function map<T>(str:String, cb:String->T):Array<T> {
+		return toArray(str).map(cb);
 	}
 
-	public static function parseFloat( f : Float, ?precision : Null<Int> ) : String {
-	    if( precision == null )
-	        return Std.string( f );
-	    if( precision < 0 )
+	public static function parseFloat(f:Float, ?precision:Null<Int>):String {
+		if (precision == null)
+			return Std.string(f);
+		if (precision < 0)
 			return throw 'invalid precision';
-		var s = Std.string( f );
-        var i = s.indexOf( '.' );
-        if( -1 == i )
+		var s = Std.string(f);
+		var i = s.indexOf('.');
+		if (-1 == i)
 			return s;
-        if( precision == 0 )
-            return s.substr( 0, i );
-        return s.substr( 0, i + 1 + precision );
-    }
-
-	public static function quote( str : String ) : String {
-		return
-			if( str.indexOf( '"' ) < 0 ) '"' + str + '"';
-			else if( str.indexOf( "'" ) < 0 ) "'" + str + "'";
-			else '"' + StringTools.replace( str, '"', '\\"' ) + '"';
+		if (precision == 0)
+			return s.substr(0, i);
+		return s.substr(0, i + 1 + precision);
 	}
 
-	public static inline function removeLinebreaks( s : String ) : String {
-		return s.split( '\n' ).join( '' );
+	public static function quote(str:String):String {
+		return if (str.indexOf('"') < 0) '"' + str + '"'; else if (str.indexOf("'") < 0) "'" + str + "'"; else '"' + StringTools.replace(str, '"', '\\"') + '"';
 	}
 
-	public static inline function repeat( s : String, n : Int ) : String {
-		return [for(i in 0...n) s].join( '' );
+	public static inline function removeLinebreaks(s:String):String {
+		return s.split('\n').join('');
+	}
+
+	public static inline function repeat(s:String, n:Int):String {
+		return [for (i in 0...n) s].join('');
 	}
 
 	/*
-	public static function replace( str : String, expr : String, newStr : String ) : String {
-		#if js
-		return untyped str.replace( expr, newStr );
-	}
-	*/
-
-	public static function reverse( str : String ) : String {
-		var a = toArray( str );
-    	a.reverse();
-    	return a.join( '' );
-	}
-
-	public static inline function split( str : String, delimiter = '' ) : Array<String> {
-		return str.split( delimiter );
+		public static function replace( str : String, expr : String, newStr : String ) : String {
+			#if js
+			return untyped str.replace( expr, newStr );
+		}
+	 */
+	public static function reverse(str:String):String {
+		var a = toArray(str);
+		a.reverse();
+		return a.join('');
 	}
 
-	public static inline function toArray( str : String, delimiter = '' ) : Array<String> {
-		return str.split( delimiter );
+	public static inline function split(str:String, delimiter = ''):Array<String> {
+		return str.split(delimiter);
 	}
 
-	public static function toIntArray( s : String ) : Array<Int> {
+	public static inline function toArray(str:String, delimiter = ''):Array<String> {
+		return str.split(delimiter);
+	}
+
+	public static function toIntArray(s:String):Array<Int> {
 		var r = new Array<Int>();
-		for( i in 0...s.length )
-			r.push( s.charCodeAt( i ) );
+		for (i in 0...s.length)
+			r.push(s.charCodeAt(i));
 		return r;
 	}
 
-	#if js
-
-	public static function createRandomString( length : Int ) : String {
-		#if doc_gen
-		return null;
-		#elseif nodejs
-        return js.node.Crypto.randomBytes( length ).toString( 'hex' ).substr( 0, length );
-        #elseif js
-        var charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-		#if (haxe_ver >= 4)
-        var values = new js.lib.Uint32Array( length );
-		#else
-        var values = new js.html.Uint32Array( length );
-		#end
-        om.Browser.window.crypto.getRandomValues( values );
-        var str = "";
-        for( i in 0...length ) str += charset.charAt( values[i] % charset.length );
-        return str;
-		#else #error
-        #end
-    }
-
-	#end
-
+	// #if js
+	// public static function createRandomString(length:Int):String {
+	//	#if doc_gen
+	//	return null;
+	//	#elseif nodejs
+	//	return js.node.Crypto.randomBytes(length).toString('hex').substr(0, length);
+	//	#elseif js
+	//	var charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+	//	#if (haxe_ver >= 4)
+	//	var values = new js.lib.Uint32Array(length);
+	//	#else
+	//	var values = new js.html.Uint32Array(length);
+	//	#end
+	//	om.Browser.window.crypto.getRandomValues(values);
+	//	var str = "";
+	//	for (i in 0...length)
+	//		str += charset.charAt(values[i] % charset.length);
+	//	return str;
+	//	#else
+	//	#error
+	//	#end
+	// }
+	// #end
 }
