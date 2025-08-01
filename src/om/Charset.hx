7c81ab379dc8adf9113a3d9b1ec5900decee02b2
tong <tong@disktree.net>
2025-08-01 11:57:55 +0200

    update charset

--- src/om/Charset.hx
+++ src/om/Charset.hx
@@ -1,30 +1,46 @@
 package om;
 
-//TODO: https://github.com/HeapsIO/heaps/blob/master/hxd/Charset.hx
-
 class Charset {
-
 	/**
 		Contains the whole ASCII charset.
 	**/
-	public static var ASCII = " !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~";
+	public static final ASCII = " !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~";
+
+	/**
+		The Latin1 (ISO 8859-1) charset (only the extra chars, no the ASCII part) + euro symbol
+	**/
+	public static final LATIN1 = "¡¢£¤¥¦§¨©ª«¬-®¯°±²³´µ¶·¸¹º»¼½¾¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûüýþÿ€";
 
 	/**
 		Russian support
 	**/
-	public static var CYRILLIC = "АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдежзийклмнопрстуфхцчшщъыьэюя—";
+	public static final CYRILLIC = "АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдежзийклмнопрстуфхцчшщъыьэюя—";
 
-    /**
+	/**
+		Polish support
+	**/
+	public static final POLISH = "ĄĆĘŁŃÓŚŹŻąćęłńóśźż";
+
+	/**
+		Turkish support
+	**/
+	public static final TURKISH = "ÂÇĞIİÎÖŞÜÛâçğıİîöşüû";
+
+	/**
 		Contains Hiragana, Katanaga, japanese punctuaction and full width space (0x3000) full width numbers (0-9) and some full width ascii punctuation (!:?%&()-).
-        Does not include full width A-Za-z.
+		Does not include full width A-Za-z.
 	**/
-	public static var JP_KANA = "　あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわゐゑをんがぎぐげござじずぜぞだぢづでどばびぶべぼぱぴぷぺぽゃゅょアイウエオカキクケコサシスセソタチツテトナニヌネノハヒフヘホマミムメモヤユヨラリルレロワヰヱヲンガギグゲゴザジズゼゾダヂヅデドバビブベボパピプペポヴャぇっッュョァィゥェォ・ー「」、。『』“”！：？％＆（）－０１２３４５６７８９";
+	public static final JP_KANA = "　あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわゐゑをんがぎぐげござじずぜぞだぢづでどばびぶべぼぱぴぷぺぽゃゅょアイウエオカキクケコサシスセソタチツテトナニヌネノハヒフヘホマミムメモヤユヨラリルレロワヰヱヲンガギグゲゴザジズゼゾダヂヅデドバビブベボパピプペポヴャぇっッュョァィゥェォ・ー「」、。『』“”！：？％＆（）－０１２３４５６７８９";
 
-    /**
-		The Latin1 (ISO 8859-1) charset (only the extra chars, no the ASCII part) + euro symbol
+	/**
+		Special unicode chars (fallback chars)
 	**/
-	public static var LATIN1 = "¡¢£¤¥¦§¨©ª«¬-®¯°±²³´µ¶·¸¹º»¼½¾¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûüýþÿ€";
+	public static final UNICODE_SPECIALS = "�□";
 
-    //public static var DEFAULT_CHARS = ASCII + LATIN1;
+	public static inline function isSpace(code:Int):Bool
+		return code == ' '.code || code == 0x3000;
 
+	// public static inline function isBreakChar(code) {
+	//	return isSpace(code) || isCJK(code);
+	// }
 }
