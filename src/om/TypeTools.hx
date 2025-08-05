148c2e6f8650433c572c09e6dda29aec60348fe8
tong <tong@disktree.net>
2025-08-05 13:56:54 +0200

    add TypeTools

---
+++ src/om/TypeTools.hx
@@ -0,0 +1,66 @@
+package om;
+
+class TypeTools {
+	/**
+		`isAnonymousObject` returns true if `v` is an object and it is not an instance of any custom class.
+	**/
+	public static inline function isAnonymousObject(v:Dynamic):Bool
+		return Reflect.isObject(v) && null == Type.getClass(v);
+
+	/**
+		Returns `true` if `v` is an instance of any Enum type.
+	**/
+	public static function isEnumValue(v:Dynamic):Bool
+		return switch Type.typeof(v) {
+			case TEnum(_): true;
+			case _: false;
+		}
+
+	/**
+		Returns `true` if the passed value is an anonymous object or class instance but it is not any of the primitive types.
+	**/
+	public static inline function isObject(v:Dynamic):Bool
+		return Reflect.isObject(v) && !isPrimitive(v);
+
+	/**
+		Returns `true` if `v` is any of the following types: Int, Float, Bool, Date or String.
+	**/
+	public static function isPrimitive(v:Dynamic):Bool
+		return switch Type.typeof(v) {
+			case TInt, TFloat, TBool: true;
+			case TNull, TFunction, TEnum(_), TObject, TUnknown: false;
+			case TClass(c) if (Type.getClassName(c) == "String"): true;
+			case TClass(c) if (Type.getClassName(c) == "Date"): true;
+			case TClass(_): false;
+		};
+
+	/**
+		Returns `true` if `cls` extends `sup` or one of its children.
+
+		It also returns `true` if `cls` and `sup` are the same.
+	**/
+	public static function hasSuperClass(cls:Class<Dynamic>, sup:Class<Dynamic>) {
+		while (null != cls) {
+			if (cls == sup)
+				return true;
+			cls = Type.getSuperClass(cls);
+		}
+		return false;
+	}
+
+	/**
+		Returns a string representation of a `ValueType`.
+	**/
+	public static function toString(type:Type.ValueType):String
+		return switch type {
+			case TNull: "Null";
+			case TInt: "Int";
+			case TFloat: "Float";
+			case TBool: "Bool";
+			case TObject: "{}";
+			case TFunction: "Function";
+			case TClass(c): Type.getClassName(c);
+			case TEnum(e): Type.getEnumName(e);
+			case _: throw 'invalid type $type';
+		}
+}
